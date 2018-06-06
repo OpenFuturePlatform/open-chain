878f7bda22d6f76f0c53fb59f5024d5960993159
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-06-06 11:48:51 +0300

    Get timestamp is added

--- src/main/kotlin/io/openfuture/chain/controller/NodeInfoController.kt
+++ src/main/kotlin/io/openfuture/chain/controller/NodeInfoController.kt
@@ -1,10 +1,12 @@
 package io.openfuture.chain.controller
 
-import io.openfuture.chain.domain.NodeVersionResponse
+import io.openfuture.chain.domain.node.response.NodeTimestampResponse
+import io.openfuture.chain.domain.node.response.NodeVersionResponse
 import io.openfuture.chain.property.NodeProperties
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("${PathConstant.RPC}/info")
@@ -12,5 +14,8 @@ class NodeInfoController(val nodeProperties: NodeProperties) {
 
     @GetMapping("/getVersion")
     fun getVersion() = NodeVersionResponse(nodeProperties.version!!)
+
+    @GetMapping("/getTimestamp")
+    fun getTimestamp() = NodeTimestampResponse(Date().time)
 
 }
---
+++ src/main/kotlin/io/openfuture/chain/domain/node/response/NodeTimestampResponse.kt
@@ -0,0 +1,3 @@
+package io.openfuture.chain.domain.node.response
+
+data class NodeTimestampResponse(val timestamp: Long)
