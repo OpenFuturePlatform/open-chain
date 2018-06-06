b5ae971e471f7eb329c038de7a08e90ddac7db44
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-06-06 12:49:09 +0300

    Getting cpu code count, cpu temperature, ram total is implemented

--- src/main/kotlin/io/openfuture/chain/controller/NodeInfoController.kt
+++ src/main/kotlin/io/openfuture/chain/controller/NodeInfoController.kt
@@ -1,16 +1,26 @@
 package io.openfuture.chain.controller
 
+import io.openfuture.chain.domain.NodeHardwareResponse
 import io.openfuture.chain.domain.NodeVersionResponse
 import io.openfuture.chain.property.NodeProperties
+import io.openfuture.chain.sevice.HardwareInfoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("${PathConstant.RPC}/info")
-class NodeInfoController(val nodeProperties: NodeProperties) {
+class NodeInfoController(
+        val hardwareInfoService: HardwareInfoService,
+        val nodeProperties: NodeProperties
+) {
 
     @GetMapping("/getVersion")
     fun getVersion() = NodeVersionResponse(nodeProperties.version!!)
+
+    @GetMapping("/getHardware")
+    fun getHardware(): NodeHardwareResponse {
+        return hardwareInfoService.getHardwareInfo()
+    }
 
 }
