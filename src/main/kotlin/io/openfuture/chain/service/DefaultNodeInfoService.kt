408a35b8f7ca26483770eb5a52d6755a50d793a7
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-06-12 16:26:22 +0300

    DefaultNodeInfoService import removed

--- src/main/kotlin/io/openfuture/chain/service/DefaultNodeInfoService.kt
+++ src/main/kotlin/io/openfuture/chain/service/DefaultNodeInfoService.kt
@@ -1,6 +1,5 @@
 package io.openfuture.chain.service
 
-import io.openfuture.chain.sevice.NodeInfoService
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
 
