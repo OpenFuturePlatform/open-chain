6a0d7a20cebf560a3e63bf54e3330c23cd70e4fd
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-06-13 09:41:36 +0300

    Controller tests is added

--- src/test/kotlin/io/openfuture/chain/controller/NodeInfoControllerTests.kt
+++ src/test/kotlin/io/openfuture/chain/controller/NodeInfoControllerTests.kt
@@ -1,6 +1,9 @@
 package io.openfuture.chain.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.openfuture.chain.config.ControllerTests
+import io.openfuture.chain.domain.NodeTimestampResponse
+import io.openfuture.chain.domain.NodeVersionResponse
 import io.openfuture.chain.property.NodeProperties
 import io.openfuture.chain.util.AppContextUtils
 import org.assertj.core.api.Assertions.assertThat
@@ -20,36 +23,51 @@ class NodeInfoControllerTests : ControllerTests() {
     @Autowired
     private lateinit var context: ApplicationContext
 
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
     @MockBean
     private lateinit var nodeProperties: NodeProperties
 
     @Before
     fun setUp() {
+        val version = "1"
         AppContextUtils.context = this.context
+        given(nodeProperties.version).willReturn(version)
     }
 
     @Test
     fun testGetVersion() {
-        val testVersion = "1"
-        val testContentValue = "{\"version\":\"1\"}"
+        val response = NodeVersionResponse()
 
-        given(nodeProperties.version).willReturn(testVersion)
+        val responseJson = mvc.perform(get("${PathConstant.RPC}/info/getVersion"))
+                .andExpect(status().isOk)
+                .andReturn().response.contentAsString
 
-        val content = mvc.perform(get("${PathConstant.RPC}/info/getVersion"))
-            .andExpect(status().isOk)
-            .andReturn().response.contentAsString
-
-        assertThat(content).isEqualTo(testContentValue)
+        assertThat(responseJson).isEqualTo(objectMapper.writeValueAsString(response))
     }
 
     @Test
     fun testGetTimestamp() {
+        val response = NodeTimestampResponse(System.currentTimeMillis())
 
+        val responseJson = mvc.perform(get("${PathConstant.RPC}/info/getTimestamp"))
+                .andExpect(status().isOk)
+                .andReturn().response.contentAsString
+        val responseResult = objectMapper.readValue(responseJson, NodeTimestampResponse::class.java)
+
+        assertThat(responseResult.version).isEqualTo(response.version)
+        assertThat(responseResult.timestamp).isGreaterThanOrEqualTo(response.timestamp)
     }
 
     @Test
     fun testGetHealthCheck() {
+        val timeUpString = mvc.perform(get("${PathConstant.RPC}/info/getHealthCheck"))
+                .andExpect(status().isOk)
+                .andReturn().response.contentAsString
+        val timeUp = timeUpString.toLong()
 
+        assertThat(timeUp).isGreaterThan(0L)
     }
 
 }
