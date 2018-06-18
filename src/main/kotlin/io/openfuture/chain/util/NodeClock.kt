1fcd20e16f800af3849059257aeadef8b2882f4b
vadzim.marchanka.by <vadzim.marchanka@zensoft.io>
2018-06-18 11:03:01 +0300

    fixed lock logic and statement when we need to do sync

--- src/main/kotlin/io/openfuture/chain/util/NodeClock.kt
+++ src/main/kotlin/io/openfuture/chain/util/NodeClock.kt
@@ -3,14 +3,16 @@ package io.openfuture.chain.util
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.locks.Lock
-import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.locks.ReadWriteLock
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 @Component
 class NodeClock {
 
     companion object {
         private val log = LoggerFactory.getLogger(NodeClock::class.java)
+
+        private const val MINIMUM_OFFSET_SIZE_TO_DO_SYNC = 5
     }
 
     @Volatile
@@ -18,32 +20,40 @@ class NodeClock {
 
     private val networkTimeOffsets: ConcurrentHashMap<String, Long> = ConcurrentHashMap()
 
-    private val lock: Lock = ReentrantLock()
+    private val lock: ReadWriteLock = ReentrantReadWriteLock()
 
     fun nodeTime() : Long = System.currentTimeMillis()
 
     fun networkTime() : Long {
-        return nodeTime() + adjustment
+        lock.readLock().lock()
+        try{
+            return nodeTime() + adjustment
+        } finally {
+            lock.readLock().unlock()
+        }
     }
 
     fun addTimeOffset(remoteAddress: String, offset: Long) {
+        lock.writeLock().lock()
         networkTimeOffsets[remoteAddress] = offset
         recalculateAdjustment()
+        lock.writeLock().unlock()
     }
 
     fun removeTimeOffset(remoteAddress: String) {
+        lock.writeLock().lock()
         networkTimeOffsets.remove(remoteAddress)
         recalculateAdjustment()
+        lock.writeLock().unlock()
     }
 
     private fun recalculateAdjustment() {
-        lock.lock()
-        if (networkTimeOffsets.size % 2 == 1 && networkTimeOffsets.size > 2) {
+        if (networkTimeOffsets.size % 2 == 1 &&
+                networkTimeOffsets.size >= MINIMUM_OFFSET_SIZE_TO_DO_SYNC) {
             val offsetList = ArrayList(networkTimeOffsets.values)
             offsetList.sort()
             adjustment = offsetList[networkTimeOffsets.size / 2]
             log.info("Time adjustment was changed: $adjustment")
         }
-        lock.unlock()
     }
 }
