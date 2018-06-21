261d4e659415cb7aece5045690ef615c93938791
Vadzim Marchanka <vadzim.marchanka@zensoft.io>
2018-06-21 14:22:57 +0300

    Fixed class formatting

--- src/main/kotlin/io/openfuture/chain/component/NodeClock.kt
+++ src/main/kotlin/io/openfuture/chain/component/NodeClock.kt
@@ -62,4 +62,5 @@ class NodeClock {
             log.info("Time adjustment was changed: $adjustment")
         }
     }
+
 }
--- src/main/kotlin/io/openfuture/chain/nio/server/handler/TimeSyncServerHandler.kt
+++ src/main/kotlin/io/openfuture/chain/nio/server/handler/TimeSyncServerHandler.kt
@@ -24,4 +24,5 @@ class TimeSyncServerHandler(
                 .build()
         ctx.channel().writeAndFlush(response)
     }
+
 }
