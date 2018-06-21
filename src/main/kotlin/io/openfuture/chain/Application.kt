f594619e1fab065d5ca51b821274e7ccd7fbcca3
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-06-21 17:43:36 +0300

    Application class is reverted

--- src/main/kotlin/io/openfuture/chain/Application.kt
+++ src/main/kotlin/io/openfuture/chain/Application.kt
@@ -1,12 +1,10 @@
 package io.openfuture.chain
 
-import org.apache.commons.io.FileUtils
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
-import java.io.File
 
 @SpringBootApplication
 class Application {
