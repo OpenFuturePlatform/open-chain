3ce1971c64395695c957fad4fed280cb69a1578b
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-06-21 12:59:07 +0300

    Test application.properties is removed

--- src/test/kotlin/io/openfuture/chain/controller/NodeInfoControllerTests.kt
+++ src/test/kotlin/io/openfuture/chain/controller/NodeInfoControllerTests.kt
@@ -1,18 +1,30 @@
 package io.openfuture.chain.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.openfuture.chain.config.ControllerTests
+import io.openfuture.chain.domain.HardwareInfo
 import io.openfuture.chain.domain.HealthResponse
+import io.openfuture.chain.domain.hardware.CpuInfo
+import io.openfuture.chain.domain.hardware.NetworkInfo
+import io.openfuture.chain.domain.hardware.RamInfo
 import io.openfuture.chain.domain.node.NodeTimestampResponse
+import io.openfuture.chain.service.HardwareInfoService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import java.util.*
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebFluxTest(NodeInfoController::class)
 class NodeInfoControllerTests : ControllerTests() {
+
+    @MockBean
+    private lateinit var hardwareInfoService: HardwareInfoService
 
     @Test
     fun getVersionShouldReturnVersion() {
-        val responseString = """{"version":"1.0.0"}"""
+        val responseString = """{"version":"$version"}"""
 
         webClient.get().uri("${PathConstant.RPC}/info/getVersion")
                 .exchange()
@@ -35,7 +47,7 @@ class NodeInfoControllerTests : ControllerTests() {
     }
 
     @Test
-    fun testGetHealthCheckShoutReturnAppUpTime() {
+    fun getHealthCheckShoutReturnAppUpTime() {
         val response = HealthResponse(1L)
 
         val responseResult = webClient.get().uri("${PathConstant.RPC}/info/getHealthCheck")
@@ -46,6 +58,36 @@ class NodeInfoControllerTests : ControllerTests() {
 
         assertThat(responseResult).isNotNull
         assertThat(response.uptime).isGreaterThan(0L)
+    }
+
+    @Test
+    fun getHardwareInfoShoutReturnHardwareInfo() {
+        val cpuModel = "1"
+        val cpuFrequency = 1L
+        val numberOfCores = 1
+        val cpuInfo = CpuInfo(cpuModel, cpuFrequency, numberOfCores)
+
+        val ramFree = 1L
+        val ramUsed = 1L
+        val ramTotal = 2L
+        val ram = RamInfo(ramFree, ramUsed, ramTotal)
+
+        val totalStorageSize = 1L
+
+        val networkInterfaceName = "IN"
+        val networkAddress = "192.168.1.1"
+        val networkAddresses = Arrays.asList(networkAddress)
+        val network = NetworkInfo(networkInterfaceName, networkAddresses)
+        val networks = Arrays.asList(network)
+
+        val hardwareInfo = HardwareInfo(cpuInfo, ram, totalStorageSize, networks)
+
+        given(hardwareInfoService.getHardwareInfo()).willReturn(hardwareInfo)
+
+        webClient.get().uri("${PathConstant.RPC}/info/getHardwareInfo")
+                .exchange()
+                .expectStatus().isOk
+                .expectBody(String::class.java).isEqualTo<Nothing>(ObjectMapper().writeValueAsString(hardwareInfo))
     }
 
 }
