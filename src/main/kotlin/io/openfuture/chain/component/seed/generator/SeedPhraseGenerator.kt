f852535a16a23230661668d8332792562b2ede6b
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-06-22 16:36:24 +0300

    nextWordsIndex method is replced to SeedPhraseGenerator

--- src/main/kotlin/io/openfuture/chain/component/seed/generator/SeedPhraseGenerator.kt
+++ src/main/kotlin/io/openfuture/chain/component/seed/generator/SeedPhraseGenerator.kt
@@ -20,6 +20,12 @@ class SeedPhraseGenerator(
 
         private const val MULTIPLICITY_VALUE = 32
 
+        private const val DOUBLE_BYTE_SIZE = 16
+
+        private const val BYTE_MASK = 0xff
+
+        private const val MAX_BYTES_TO_READ = 3
+
     }
 
     fun createSeedPhrase(length: PhraseLength): String {
@@ -45,11 +51,27 @@ class SeedPhraseGenerator(
         var bitOffset = 0
         var wordIndex = 0
         while (wordIndex < mnemonicLength) {
-            wordIndexes[wordIndex] = HashUtils.nextWordsIndex(entropyWithChecksum, bitOffset)
+            wordIndexes[wordIndex] = nextWordsIndex(entropyWithChecksum, bitOffset)
             bitOffset += SeedGeneratorConstant.WORD_INDEX_SIZE
             wordIndex++
         }
         return wordIndexes
     }
 
+    private fun nextWordsIndex(bytes: ByteArray, offset: Int): Int {
+        val skip = offset / SeedGeneratorConstant.BYTE_SIZE
+        val lowerBitsToRemove = (MAX_BYTES_TO_READ * SeedGeneratorConstant.BYTE_SIZE
+                - SeedGeneratorConstant.WORD_INDEX_SIZE) - (offset % SeedGeneratorConstant.BYTE_SIZE)
+
+        val firstBytePart = bytes[skip].toInt() and BYTE_MASK shl DOUBLE_BYTE_SIZE
+        val secondBytePart = bytes[skip + 1].toInt() and BYTE_MASK shl SeedGeneratorConstant.BYTE_SIZE
+        var thirdBytePart = 0
+        if (lowerBitsToRemove < SeedGeneratorConstant.BYTE_SIZE) {
+            thirdBytePart = bytes[skip + 2].toInt() and BYTE_MASK
+        }
+        return (
+                (((firstBytePart or secondBytePart or thirdBytePart)) shr lowerBitsToRemove)
+                        and (1 shl SeedGeneratorConstant.WORD_INDEX_SIZE) - 1
+                )
+    }
 }
--- src/main/kotlin/io/openfuture/chain/util/HashUtils.kt
+++ src/main/kotlin/io/openfuture/chain/util/HashUtils.kt
@@ -1,37 +1,13 @@
 package io.openfuture.chain.util
 
-import io.openfuture.chain.component.seed.SeedGeneratorConstant
 import java.security.MessageDigest
 
 object HashUtils {
-
-	private const val DOUBLE_BYTE_SIZE = 16
-
-	private const val BYTE_MASK = 0xff
-
-	private const val MAX_BYTES_TO_READ = 3
 
 	fun generateHash(bytes: ByteArray): String {
 		val instance = MessageDigest.getInstance("SHA-256")
 		val digest = instance.digest(bytes)
 		return digest.fold("") { str, it -> str + "%02x".format(it) }
-	}
-
-	fun nextWordsIndex(bytes: ByteArray, offset: Int): Int {
-		val skip = offset / SeedGeneratorConstant.BYTE_SIZE
-        val lowerBitsToRemove = (MAX_BYTES_TO_READ * SeedGeneratorConstant.BYTE_SIZE
-				- SeedGeneratorConstant.WORD_INDEX_SIZE) - (offset % SeedGeneratorConstant.BYTE_SIZE)
-
-		val firstBytePart = bytes[skip].toInt() and BYTE_MASK shl DOUBLE_BYTE_SIZE
-		val secondBytePart = bytes[skip + 1].toInt() and BYTE_MASK shl SeedGeneratorConstant.BYTE_SIZE
-		var thirdBytePart = 0
-		if (lowerBitsToRemove < SeedGeneratorConstant.BYTE_SIZE) {
-			thirdBytePart = bytes[skip + 2].toInt() and BYTE_MASK
-		}
-        return (
-			(((firstBytePart or secondBytePart or thirdBytePart)) shr lowerBitsToRemove)
-					and (1 shl SeedGeneratorConstant.WORD_INDEX_SIZE) - 1
-		)
 	}
 
 	fun sha256(bytes: ByteArray): ByteArray {
