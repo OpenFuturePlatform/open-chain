fab0e886b19214b51683cf094caf1c58ef0ca9eb
anna.shunko.by <anna.shunko@zensoft.io>
2018-06-27 10:44:54 +0300

    OP-96: simple derivation realisation added

--- src/main/kotlin/io/openfuture/chain/domain/key/ExtendedKey.kt
+++ src/main/kotlin/io/openfuture/chain/domain/key/ExtendedKey.kt
@@ -1,14 +1,17 @@
 package io.openfuture.chain.domain.key
 
+import io.openfuture.chain.util.HashUtils
 import java.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 class ExtendedKey {
 
-    private var chainCode: ByteArray
-    private var ecKey: ECKey
-    private var sequence: Int = 0
-    private var depth: Int = 0
-    private var parentFingerprint: Int = 0
+    var chainCode: ByteArray
+    var ecKey: ECKey
+    var sequence: Int = 0
+    var depth: Int = 0
+    var parentFingerprint: Int = 0
 
     constructor(
         keyHash: ByteArray,
@@ -23,15 +26,41 @@ class ExtendedKey {
         this.sequence = sequence
         this.depth = depth
         this.parentFingerprint = parentFingerprint
-
-        if (ecKey != null) {
-            this.ecKey = ECKey(leftBytes, ecKey)
-        } else {
-            this.ecKey = ECKey(leftBytes)
-        }
+        this.ecKey = ecKey?.let { ECKey(leftBytes, it) } ?: ECKey(leftBytes)
     }
 
-    //TODO: implement derivation
-    fun derive(i: Int) = ExtendedKey(ByteArray(0))
+    /**
+     * Currently supports only simple derivation m/i
+     * Accounts with internal/external key chains need to be implemented
+     */
+    fun derive(i: Int) = getChild(i)
+
+    private fun getChild(i: Int): ExtendedKey {
+        val mac = Mac.getInstance("HmacSHA512", "BC")
+        val key = SecretKeySpec(chainCode, "HmacSHA512")
+        mac.init(key)
+        val public = this.ecKey.public!!
+        val child = ByteArray(public.size + 4)
+        System.arraycopy(public, 0, child, 0, public.size)
+        child[public.size] = (i.ushr(24) and 0xff).toByte()
+        child[public.size + 1] = (i.ushr(16) and 0xff).toByte()
+        child[public.size + 2] = (i.ushr(8) and 0xff).toByte()
+        child[public.size + 3] = (i and 0xff).toByte()
+        val keyHash = mac.doFinal(child)
+        return ExtendedKey(keyHash, i, this.depth + 1, getFingerprint(), this.ecKey)
+    }
+
+    /**
+     *  RIPE-MD160(SHA256(public key bytes))
+     */
+    fun getFingerprint(): Int {
+        val keyHash = HashUtils.keyHash(ecKey.public!!)
+        var fingerprint = 0
+        for (i in 0..3) {
+            fingerprint = fingerprint shl 8
+            fingerprint = fingerprint or (keyHash[i].toInt() and 0xff)
+        }
+        return fingerprint
+    }
 
 }
--- src/main/kotlin/io/openfuture/chain/util/HashUtils.kt
+++ src/main/kotlin/io/openfuture/chain/util/HashUtils.kt
@@ -1,13 +1,25 @@
 package io.openfuture.chain.util
 
+import org.bouncycastle.crypto.digests.RIPEMD160Digest
 import java.security.MessageDigest
 
 object HashUtils {
 
-	fun generateHash(bytes: ByteArray): String {
-		val instance = MessageDigest.getInstance("SHA-256")
-		val digest = instance.digest(bytes)
-		return digest.fold("", { str, it -> str + "%02x".format(it) })
-	}
+    private val SHA256 = "SHA-256"
+
+    fun generateHash(bytes: ByteArray): String {
+        val instance = MessageDigest.getInstance(SHA256)
+        val digest = instance.digest(bytes)
+        return digest.fold("", { str, it -> str + "%02x".format(it) })
+    }
+
+    fun keyHash(bytes: ByteArray): ByteArray {
+        val result = ByteArray(20)
+        val sha256 = MessageDigest.getInstance(SHA256).digest(bytes)
+        val digest = RIPEMD160Digest()
+        digest.update(sha256, 0, sha256.size)
+        digest.doFinal(result, 0)
+        return result
+    }
 
 }
