ae92a111403060a7c85ece805a2ec9615f4adb20
anna.shunko.by <anna.shunko@zensoft.io>
2018-06-27 15:26:42 +0300

    OP-96: Importing private key

--- src/main/kotlin/io/openfuture/chain/component/PrivateKeyManager.kt
+++ src/main/kotlin/io/openfuture/chain/component/PrivateKeyManager.kt
@@ -4,14 +4,18 @@ import io.openfuture.chain.domain.key.ECKey
 import io.openfuture.chain.util.Base58
 import io.openfuture.chain.util.HashUtils
 import org.springframework.stereotype.Component
+import java.util.*
 
+/**
+ * Component for exporting/importing private keys in WIF (Wallet Import Format) - a Base58 String representation
+ * of private key
+ */
 @Component
-class KeyExporter {
+class PrivateKeyManager {
 
-    /**
-     * Exporting in a Wallet Import Format - a Base58 String representation of private key
-     */
     fun exportPrivateKey(key: ECKey): String = Base58.encode(getWIFBytes(key))
+
+    fun importPrivateKey(serializedKey: String): ECKey = parseWIFBytes(Base58.decode(serializedKey))
 
     private fun getWIFBytes(key: ECKey): ByteArray {
         return key.private?.let {
@@ -28,6 +32,23 @@ class KeyExporter {
             System.arraycopy(checkSum, 0, result, extendedKey.size, 4)
             result
         } ?: throw IllegalStateException("Unable to provide WIF if no private key is present")
+    }
+
+    private fun parseWIFBytes(keyBytes: ByteArray): ECKey {
+        checkChecksum(keyBytes)
+        if (keyBytes.size == 38) {
+            val key = Arrays.copyOfRange(keyBytes, 1, keyBytes.size - 5)
+            return ECKey(key, true)
+        }
+        throw IllegalArgumentException("Invalid key length")
+    }
+
+    private fun checkChecksum(bytes: ByteArray) {
+        val keyBytes = Arrays.copyOfRange(bytes, 0, bytes.size - 4)
+        val checksum = Arrays.copyOfRange(bytes, bytes.size - 4, bytes.size)
+        val actualChecksum = HashUtils.generateHashBytes(keyBytes)
+        if (!Arrays.equals(checksum, actualChecksum))
+            throw IllegalArgumentException("Invalid checksum")
     }
 
 }
