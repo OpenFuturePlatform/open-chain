bd7a957c38dd6b2adbdd9d0abf567d1d7b4b4d47
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-06-27 10:36:16 +0300

    Refactored ECKey entity

--- src/main/kotlin/io/openfuture/chain/domain/key/ECKey.kt
+++ src/main/kotlin/io/openfuture/chain/domain/key/ECKey.kt
@@ -15,9 +15,9 @@ class ECKey {
     constructor(bytes: ByteArray) : this(bytes, true)
 
     constructor(left: ByteArray, parent: ECKey) {
-        if (parent.hasPrivate()) {
-            this.private = BigInteger(1, left).add(parent.private!!).mod(curve.n)
-            setPublic(true, null)
+        if (null != parent.private) {
+            private = BigInteger(1, left).add(parent.private!!).mod(curve.n)
+            public = curve.g.multiply(private).encoded
         } else {
             throw Error("Support derived ECKey with public key only")
         }
@@ -26,19 +26,9 @@ class ECKey {
     constructor(bytes: ByteArray, isPrivate: Boolean) {
         if (isPrivate) {
             this.private = BigInteger(1, bytes)
-            setPublic(true, null)
+            public = curve.g.multiply(private).encoded
         } else {
-            setPublic(false, bytes)
-        }
-    }
-
-    private fun hasPrivate() = null != private
-
-    private fun setPublic(fromPrivate: Boolean, bytes: ByteArray?) {
-        public = if (fromPrivate) {
-            curve.g.multiply(private).encoded
-        } else {
-            bytes
+            public = bytes
         }
     }
 
