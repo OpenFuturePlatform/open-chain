38d76db9ab03f45acad13cf41d6c0875e880d3c4
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-06-29 15:57:01 +0300

    Added unit test to generateKey method

--- src/test/kotlin/io/openfuture/chain/service/CryptoServiceTests.kt
+++ src/test/kotlin/io/openfuture/chain/service/CryptoServiceTests.kt
@@ -1,9 +1,11 @@
 package io.openfuture.chain.service
 
 import io.openfuture.chain.config.ServiceTests
+import io.openfuture.chain.config.any
+import io.openfuture.chain.crypto.domain.ExtendedKey
 import io.openfuture.chain.crypto.key.DerivationKeysHelper
 import io.openfuture.chain.crypto.key.ExtendedKeySerializer
-import io.openfuture.chain.crypto.seed.PhraseLength
+import io.openfuture.chain.crypto.seed.PhraseLength.TWELVE
 import io.openfuture.chain.crypto.seed.generator.SeedPhraseGenerator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
@@ -25,11 +27,32 @@ class CryptoServiceTests : ServiceTests() {
     fun generateBip44SeedPhraseShouldReturnTwelveSeedPhraseWords() {
         val seedPhrase = "1 2 3 4 5 6 7 8 9 10 11 12"
 
-        given(seedPhraseGenerator.createSeedPhrase(PhraseLength.TWELVE)).willReturn(seedPhrase)
+        given(seedPhraseGenerator.createSeedPhrase(TWELVE)).willReturn(seedPhrase)
 
         val seedPhraseResult = cryptoService.generateSeedPhrase()
 
         assertThat(seedPhrase).isEqualTo(seedPhraseResult)
+    }
+
+    @Test
+    fun generateKeyShouldReturnWalletDtoTest() {
+        val seedPhrase = "ability able about above absent absorb abstract absurd abuse accident access act"
+        val privateKey  = "xprv9s21ZrQH143K4QKw9Cq9BUSUJGMSNMBt5mQVU8QD32NZpw4i6bnmiACNkqunc6P6B5tHXGw4oJMo2wXVwDgj2WDQFpTFufd4TdtKpZvpgEb"
+        val publicKey = "xpub661MyMwAqRbcGtQQFEN9YcPCrJBvmoujSzL6GWopbMuYhjPre972FxWrc6NHZiH87hAz3vg3o95GDTwncHF6dMkoJLQ897p4VssRDA4kJ7V"
+
+        given(seedPhraseGenerator.createSeedPhrase(TWELVE)).willReturn(seedPhrase)
+        given(derivationKeysHelper.deriveDefaultAddress(any(ExtendedKey::class.java))).will { invocation -> invocation.arguments[0]  }
+        given(extendedKeySerializer.serializePublic(any(ExtendedKey::class.java))).willReturn(publicKey)
+        given(extendedKeySerializer.serializePrivate(any(ExtendedKey::class.java))).willReturn(privateKey)
+
+        val actualWalletDto = cryptoService.generateKey()
+
+        assertThat(actualWalletDto.seedPhrase).isEqualTo(seedPhrase)
+        assertThat(actualWalletDto.masterPublicKey).isEqualTo(publicKey)
+        assertThat(actualWalletDto.masterPrivateKey).isEqualTo(privateKey)
+        assertThat(actualWalletDto.addressKeyDto.publicKey).isEqualTo(publicKey)
+        assertThat(actualWalletDto.addressKeyDto.privateKey).isEqualTo(privateKey)
+        assertThat(actualWalletDto.addressKeyDto.address).isNotNull()
     }
 
 }
