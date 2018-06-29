1777a9d86fd127063136fc2b4db000a3ebf1c4f6
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-06-29 13:08:42 +0300

    Added construct keyDto entity in generateKeys method

--- src/main/kotlin/io/openfuture/chain/service/DefaultCryptoService.kt
+++ src/main/kotlin/io/openfuture/chain/service/DefaultCryptoService.kt
@@ -1,23 +1,27 @@
 package io.openfuture.chain.service
 
 import io.openfuture.chain.crypto.domain.ExtendedKey
+import io.openfuture.chain.crypto.key.ExtendedKeySerializer
 import io.openfuture.chain.crypto.seed.PhraseLength.TWELVE
 import io.openfuture.chain.crypto.seed.generator.SeedPhraseGenerator
+import io.openfuture.chain.domain.crypto.key.KeyDto
 import org.springframework.stereotype.Service
 
 @Service
 class DefaultCryptoService(
-    private val seedPhraseGenerator: SeedPhraseGenerator
+    private val seedPhraseGenerator: SeedPhraseGenerator,
+    private val extendedKeySerializer: ExtendedKeySerializer
 ) : CryptoService {
 
     override fun generateSeedPhrase(): String = seedPhraseGenerator.createSeedPhrase(TWELVE)
 
-    override fun generateKey(): String {
+    override fun generateKey(): KeyDto {
         val seedPhrase = seedPhraseGenerator.createSeedPhrase(TWELVE)
 
         val rootExtendedKey = ExtendedKey.root(seedPhrase.toByteArray())
 
-        return rootExtendedKey;
+        return KeyDto(extendedKeySerializer.serializePublic(rootExtendedKey),
+                extendedKeySerializer.serializePrivate(rootExtendedKey))
     }
 
 }
--- src/main/kotlin/io/openfuture/chain/service/Services.kt
+++ src/main/kotlin/io/openfuture/chain/service/Services.kt
@@ -2,6 +2,7 @@ package io.openfuture.chain.service
 
 import io.openfuture.chain.domain.HardwareInfo
 import io.openfuture.chain.domain.block.BlockRequest
+import io.openfuture.chain.domain.crypto.key.KeyDto
 import io.openfuture.chain.domain.hardware.CpuInfo
 import io.openfuture.chain.domain.hardware.NetworkInfo
 import io.openfuture.chain.domain.hardware.RamInfo
@@ -39,7 +40,7 @@ interface CryptoService {
 
     fun generateSeedPhrase(): String
 
-    fun generateKey(): String
+    fun generateKey(): KeyDto
 
 }
 
