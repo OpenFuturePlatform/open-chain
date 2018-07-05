340ce13f540e3825e55f9e6dfa76644825befdc6
pgomza <pavel.gomza@zensoft.io>
2018-07-05 19:43:44 +0300

    OP-127: added vote type / service / logic

--- src/main/kotlin/io/openfuture/chain/service/DefaultVoteTransactionService.kt
+++ src/main/kotlin/io/openfuture/chain/service/DefaultVoteTransactionService.kt
@@ -4,7 +4,10 @@ import io.openfuture.chain.component.node.NodeClock
 import io.openfuture.chain.crypto.util.HashUtils
 import io.openfuture.chain.domain.transaction.VoteTransactionDto
 import io.openfuture.chain.domain.transaction.vote.VoteTransactionData
+import io.openfuture.chain.entity.Block
+import io.openfuture.chain.entity.Vote
 import io.openfuture.chain.entity.VoteTransaction
+import io.openfuture.chain.repository.VoteRepository
 import io.openfuture.chain.repository.VoteTransactionRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,13 +15,18 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class DefaultVoteTransactionService(
         private val nodeClock: NodeClock,
-        private val repository: VoteTransactionRepository
+        private val repository: VoteTransactionRepository,
+        private val voteRepository: VoteRepository
 ) : BaseTransactionService<VoteTransaction>(repository), VoteTransactionService {
 
     @Transactional
     override fun add(dto: VoteTransactionDto): VoteTransaction {
         //todo need to add validation
-        return repository.save(VoteTransaction.of(dto))
+
+        val persistTransaction = repository.save(VoteTransaction.of(dto))
+        val votes = dto.data.votes.map { voteRepository.save(Vote.of(persistTransaction, it)) }
+        persistTransaction.votes.addAll(votes)
+        return persistTransaction
     }
 
     override fun create(data: VoteTransactionData): VoteTransactionDto {
