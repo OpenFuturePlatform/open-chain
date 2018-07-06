457ee1b867c58357f5f532964e7a4bf1d20dc1db
anna.shunko.by <anna.shunko@zensoft.io>
2018-07-06 16:38:54 +0300

    OP-129: signature entity added

---
+++ src/main/kotlin/io/openfuture/chain/crypto/domain/ECDSASignature.kt
@@ -0,0 +1,22 @@
+package io.openfuture.chain.crypto.domain
+
+import org.bouncycastle.asn1.ASN1Integer
+import org.bouncycastle.asn1.DERSequenceGenerator
+import java.io.ByteArrayOutputStream
+import java.math.BigInteger
+
+data class ECDSASignature(
+    val r: BigInteger,
+    val s: BigInteger
+) {
+
+    fun toDER(): ByteArray {
+        val bos = ByteArrayOutputStream(72)
+        val seq = DERSequenceGenerator(bos)
+        seq.addObject(ASN1Integer(r))
+        seq.addObject(ASN1Integer(s))
+        seq.close()
+        return bos.toByteArray()
+    }
+
+}
