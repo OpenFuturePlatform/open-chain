e5a68b1b15110e65981ee482bb07533a5437ecef
Yury <yury.shakhau@zensoft.io>
2018-07-06 12:44:42 +0300

    epochIndex is added to Genesis block

--- src/main/kotlin/io/openfuture/chain/entity/GenesisBlock.kt
+++ src/main/kotlin/io/openfuture/chain/entity/GenesisBlock.kt
@@ -6,6 +6,9 @@ import javax.persistence.*
 @Table(name = "genesis_blocks")
 class GenesisBlock(hash: String, height: Long, previousHash: String, merkleHash: String, timestamp: Long,
 
+    @Column(name = "epoch_index", nullable = false)
+    var epochIndex: Long,
+
     @ElementCollection
     @CollectionTable(name="active_delegates", joinColumns=[JoinColumn(name = "genesis_block_id")])
     @Column(name="delegate_ip")
