b26eec816517654a4ab681342b0ef704c0586701
Yury <yury.shakhau@zensoft.io>
2018-07-06 11:15:17 +0300

    Application class is changed

--- src/main/kotlin/io/openfuture/chain/Application.kt
+++ src/main/kotlin/io/openfuture/chain/Application.kt
@@ -1,9 +1,5 @@
 package io.openfuture.chain
 
-import io.openfuture.chain.crypto.seed.validator.SeedPhraseValidator
-import io.openfuture.chain.service.CryptoService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
@@ -11,26 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 
 @SpringBootApplication
-class Application : CommandLineRunner {
-
-    @Autowired
-    private lateinit var cryptoService: CryptoService
-
-    @Autowired
-    private lateinit var seedPhraseValidator: SeedPhraseValidator
-
-    override fun run(vararg args: String?) {
-        val sp = cryptoService.generateSeedPhrase()
-
-        try {
-            seedPhraseValidator.validate(sp)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            throw e
-        }
-
-        println("it works")
-    }
+class Application {
 
     @Profile("local")
     @Bean
