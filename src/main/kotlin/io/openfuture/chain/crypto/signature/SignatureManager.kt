2606dd0c5f8ef0ba48c0dcebd8b81329cd157d07
anna.shunko.by <anna.shunko@zensoft.io>
2018-07-07 14:28:46 +0300

    OP-129: signature manager component added

---
+++ src/main/kotlin/io/openfuture/chain/crypto/signature/SignatureManager.kt
@@ -0,0 +1,34 @@
+package io.openfuture.chain.crypto.signature
+
+import io.openfuture.chain.crypto.domain.ECKey
+import io.openfuture.chain.crypto.util.HashUtils
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class SignatureManager {
+
+    /**
+     * Sign data with ECDSA
+     *
+     * @param data Information to sign
+     * @param privateKey Private key of signer
+     * @return Base64 encoded signature
+     */
+    fun sign(data: ByteArray, privateKey: ByteArray): String =
+        Base64.getEncoder().encodeToString(ECKey(privateKey).sign(HashUtils.sha256(data)))
+
+    /**
+     * Verify ECDSA signature
+     *
+     * @param data Information that was signed
+     * @param signature Base64 encoded signature
+     * @return True if valid signature. False otherwise
+     */
+    fun verify(data: ByteArray, signature: String, publicKey: ByteArray): Boolean {
+        val ecKey = ECKey(publicKey, false)
+        val decodedSign = Base64.getDecoder().decode(signature)
+        return ecKey.verify(HashUtils.sha256(data), decodedSign)
+    }
+
+}
