ba366acf5e41a6fbfc456445203dfa90376ee1aa
Yury <yury.shakhau@zensoft.io>
2018-07-09 18:08:26 +0300

    Packet changed

--- src/main/kotlin/io/openfuture/chain/service/Services.kt
+++ src/main/kotlin/io/openfuture/chain/service/Services.kt
@@ -64,8 +64,7 @@ interface NetworkService {
 
     fun joinNetwork(host : String, port: Int)
 
-    fun handleJoinResponse(message: CommunicationProtocol.Packet,
-                           channel: Channel)
+    fun handleJoinResponse(message: CommunicationProtocol.Packet, channel: Channel)
 
     fun connect(host : String, port: Int)
 
