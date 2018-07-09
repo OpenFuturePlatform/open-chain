542a6134dea8e29e7575842cfa3ce7595236b3ea
anna.shunko.by <anna.shunko@zensoft.io>
2018-07-09 16:43:14 +0300

    OP-129: derivation improved (hardened indexes support, purpose support)

--- src/main/kotlin/io/openfuture/chain/crypto/key/DerivationKeysHelper.kt
+++ src/main/kotlin/io/openfuture/chain/crypto/key/DerivationKeysHelper.kt
@@ -15,12 +15,17 @@ class DerivationKeysHelper {
     companion object {
         private const val PATH_SEPARATOR = '/'
         private const val HARDENED_DERIVATION_FLAG = 'h'
-        private const val CORRECT_PATH_PATTERN = "^m([/]+[0-9]+[h]?){1,3}"
+        private const val CORRECT_PATH_PATTERN = "^m([/]+[0-9]+[h]?){1,4}"
 
-        private const val ACCOUNT_NUMBER_POSITION = 1
-        private const val WALLET_NUMBER_POSITION = 2
-        private const val ADDRESS_NUMBER_POSITION = 3
+        private const val PURPOSE_NUMBER_POSITION = 1
+        private const val ACCOUNT_NUMBER_POSITION = 2
+        private const val WALLET_NUMBER_POSITION = 3
+        private const val ADDRESS_NUMBER_POSITION = 4
+
+        private const val DEFAULT_ACCOUNT_INDEX = 0
+        private const val DEFAULT_MULTISIG_ACCOUNT_INDEX = 45
     }
+
 
     fun derive(rootKey: ExtendedKey, derivationPath: String): ExtendedKey {
         if (!CORRECT_PATH_PATTERN.toRegex().matches(derivationPath)) {
@@ -28,20 +33,27 @@ class DerivationKeysHelper {
         }
 
         val separatedPath = derivationPath.split(PATH_SEPARATOR)
-        val accountNumber = translatePathNumber(separatedPath[ACCOUNT_NUMBER_POSITION])
+        val purposeNumber = translatePathIndex(separatedPath[PURPOSE_NUMBER_POSITION])
+        val purposeRoot = rootKey.derive(purposeNumber)
+
+        if (PURPOSE_NUMBER_POSITION == separatedPath.size - 1) {
+            return purposeRoot
+        }
+
+        val accountNumber = translatePathIndex(separatedPath[ACCOUNT_NUMBER_POSITION])
 
         if (ACCOUNT_NUMBER_POSITION == separatedPath.size - 1) {
-            return rootKey.derive(accountNumber)
+            return purposeRoot.derive(accountNumber)
         }
 
-        val walletNumber = translatePathNumber(separatedPath[WALLET_NUMBER_POSITION])
+        val walletNumber = translatePathIndex(separatedPath[WALLET_NUMBER_POSITION])
 
         if (WALLET_NUMBER_POSITION == separatedPath.size - 1) {
-            return deriveWalletChainKeys(rootKey, accountNumber, walletNumber)
+            return deriveWalletChainKeys(purposeRoot, accountNumber, walletNumber)
         }
 
-        val addressNumber = translatePathNumber(separatedPath[ADDRESS_NUMBER_POSITION])
-        val base = deriveWalletChainKeys(rootKey, accountNumber, walletNumber)
+        val addressNumber = translatePathIndex(separatedPath[ADDRESS_NUMBER_POSITION])
+        val base = deriveWalletChainKeys(purposeRoot, accountNumber, walletNumber)
 
         return base.derive(addressNumber)
     }
@@ -49,21 +61,26 @@ class DerivationKeysHelper {
     fun deriveDefaultAddress(rootKey: ExtendedKey): ExtendedKey {
         val base = deriveWalletChainKeys(rootKey, 0, 0)
 
-        return base.derive(0)
+        return base.derive(hardenedIndex(DEFAULT_ACCOUNT_INDEX))
     }
+
+    fun deriveDefaultMultisigAdress(rootKey: ExtendedKey): ExtendedKey =
+        rootKey.derive(hardenedIndex(DEFAULT_MULTISIG_ACCOUNT_INDEX))
 
     private fun deriveWalletChainKeys(rootKey: ExtendedKey, accountNumber: Int, walletNumber: Int): ExtendedKey {
         val base = rootKey.derive(accountNumber)
         return base.derive(walletNumber)
     }
 
+    private fun hardenedIndex(index: Int): Int = index or -0x80000000
+
     /**
      * Non hardened indexes in [0, 2^31)
-     * Hardened indexes in [2^31 2^32)
+     * Hardened indexes in [2^31, 2^32)
      */
-    private fun translatePathNumber(value: String): Int {
+    private fun translatePathIndex(value: String): Int {
         return if (value.toLowerCase().contains(HARDENED_DERIVATION_FLAG)) {
-            Integer.parseInt(value.dropLast(1)) or -0x80000000
+            hardenedIndex(Integer.parseInt(value.dropLast(1)))
         } else {
             Integer.parseInt(value)
         }
