caa5206999c05c3b786e1700933a907ba9c97918
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2018-07-10 16:46:42 +0300

    OP-150: Refactoring code

--- src/main/kotlin/io/openfuture/chain/service/DefaultEpochService.kt
+++ src/main/kotlin/io/openfuture/chain/service/DefaultEpochService.kt
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(readOnly = true)
 class DefaultEpochService(
-        private val epochProperties: EpochProperties
+    private val epochProperties: EpochProperties
 ) : EpochService {
 
     override fun getActualSizeCurrentEpoch(): Int {
