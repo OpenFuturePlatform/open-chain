c478381446346bb46c22ecbd29d65d8f2e4fcd41
pgomza <pavel.gomza@zensoft.io>
2018-07-11 11:05:07 +0300

    OP-127: merge with master

--- src/main/kotlin/io/openfuture/chain/service/stakeholder/DefaultStakeholderService.kt
+++ src/main/kotlin/io/openfuture/chain/service/stakeholder/DefaultStakeholderService.kt
@@ -5,12 +5,14 @@ import io.openfuture.chain.entity.account.Stakeholder
 import io.openfuture.chain.repository.StakeholderRepository
 import io.openfuture.chain.service.StakeholderService
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class DefaultStakeholderService (
         private val repository: StakeholderRepository<Stakeholder>
 ) : DefaultBaseStakeholderService<Stakeholder, StakeholderDto>(repository), StakeholderService {
 
+    @Transactional
     override fun add(dto: StakeholderDto): Stakeholder  = repository.save(Stakeholder.of(dto))
 
 }
