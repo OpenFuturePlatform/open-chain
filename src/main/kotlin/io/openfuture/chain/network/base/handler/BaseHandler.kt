73cadb9e9aaedcd1406ebb4f35d45a3b506c7062
Vadzim Marchanka <vadzim.marchanka@zensoft.io>
2018-07-24 09:54:38 +0300

    Replaced KClass in base handler by PacketType

--- src/main/kotlin/io/openfuture/chain/network/base/handler/BaseHandler.kt
+++ src/main/kotlin/io/openfuture/chain/network/base/handler/BaseHandler.kt
@@ -4,19 +4,22 @@ import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.util.ReferenceCountUtil
 import io.openfuture.chain.network.domain.Packet
+import io.openfuture.chain.network.domain.PacketType
+import kotlin.reflect.KClass
 
 abstract class BaseHandler<T : Packet>(
         private val autoRelease: Boolean = true
 ) : ChannelInboundHandlerAdapter() {
 
-    private val genericClass = this::class.supertypes[0].arguments[0].type!!.classifier!!
+    private val packetType = PacketType.get(this::class.supertypes[0].arguments[0].type!!.classifier!!
+        as KClass<out Packet>)
 
 
     final override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
         var release = true
         try {
-            if (msg::class == genericClass) {
-                channelRead(ctx, msg as T)
+            if (packetType == PacketType.get(msg as T)) {
+                channelRead(ctx, msg)
             } else {
                 release = false
                 ctx.fireChannelRead(msg)
