f66ae029e593d789807397878ea4dc907f76766f
anna.shunko.by <anna.shunko@zensoft.io>
2018-07-31 15:49:48 +0300

    Seed phrase validator improved

--- src/main/kotlin/io/openfuture/chain/crypto/validation/SeedPhraseValidator.kt
+++ src/main/kotlin/io/openfuture/chain/crypto/validation/SeedPhraseValidator.kt
@@ -4,7 +4,6 @@ import io.openfuture.chain.crypto.constants.SeedConstant
 import io.openfuture.chain.crypto.constants.SeedConstant.BYTE_SIZE
 import io.openfuture.chain.crypto.constants.SeedConstant.DOUBLE_BYTE_SIZE
 import io.openfuture.chain.crypto.constants.SeedConstant.SECOND_BYTE_OFFSET
-import io.openfuture.chain.crypto.constants.SeedConstant.SECOND_BYTE_SKIP_BIT_SIZE
 import io.openfuture.chain.crypto.constants.SeedConstant.THIRD_BYTE_OFFSET
 import io.openfuture.chain.crypto.constants.SeedConstant.WORD_INDEX_SIZE
 import io.openfuture.chain.crypto.model.dictionary.PhraseLength
@@ -27,6 +26,7 @@ class SeedPhraseValidator(
         private const val MAX_BYTE_SIZE_MOD = 7
         private const val OUT_OF_BYTE_SIZE = WORD_INDEX_SIZE - BYTE_SIZE
         private const val MAX_FIRST_BIT_INDEX_IN_TWO_BYTE = DOUBLE_BYTE_SIZE - WORD_INDEX_SIZE
+        private const val SECOND_BYTE_SKIP_BIT_SIZE = DOUBLE_BYTE_SIZE - WORD_INDEX_SIZE
         private const val THIRD_BYTE_SKIP_BIT_SIZE = BYTE_SIZE + MAX_FIRST_BIT_INDEX_IN_TWO_BYTE
     }
 
