534b6ff99975cca54d299c6eabfd49903e0a436a
Yury <yury.shakhau@zensoft.io>
2018-08-02 09:52:53 +0300

    One more test is added

--- src/test/kotlin/io/openfuture/chain/component/block/DefaultPendingBlockHandlerTests.kt
+++ src/test/kotlin/io/openfuture/chain/component/block/DefaultPendingBlockHandlerTests.kt
@@ -8,17 +8,15 @@ import io.openfuture.chain.consensus.model.entity.transaction.TransferTransactio
 import io.openfuture.chain.consensus.service.EpochService
 import io.openfuture.chain.core.component.NodeKeyHolder
 import io.openfuture.chain.core.model.entity.Delegate
-import io.openfuture.chain.core.model.entity.block.GenesisBlock
 import io.openfuture.chain.core.model.entity.block.MainBlock
 import io.openfuture.chain.core.model.entity.transaction.Transaction
-import io.openfuture.chain.core.service.GenesisBlockService
 import io.openfuture.chain.core.service.MainBlockService
 import io.openfuture.chain.core.util.TransactionUtils
 import io.openfuture.chain.crypto.util.SignatureUtils
 import io.openfuture.chain.network.domain.NetworkBlockApproval
 import io.openfuture.chain.network.domain.NetworkMainBlock
 import io.openfuture.chain.network.service.NetworkService
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.bouncycastle.pqc.math.linearalgebra.ByteUtils
 import org.junit.Before
 import org.junit.Test
@@ -31,7 +29,6 @@ import org.mockito.Mockito.verify
 class DefaultPendingBlockHandlerTests : ServiceTests() {
 
     @Mock private lateinit var epochService: EpochService
-    @Mock private lateinit var genesisBlockService: GenesisBlockService
     @Mock private lateinit var mainBlockService: MainBlockService
     @Mock private lateinit var keyHolder: NodeKeyHolder
     @Mock private lateinit var networkService: NetworkService
@@ -92,22 +89,13 @@ class DefaultPendingBlockHandlerTests : ServiceTests() {
 
         verify(networkService, times(1)).broadcast(any(NetworkMainBlock::class.java))
         verify(networkService, times(1)).broadcast(any(NetworkBlockApproval::class.java))
-        Assertions.assertThat(defaultPendingBlockHandler.pendingBlocks.first()).isEqualTo(block)
+        assertThat(defaultPendingBlockHandler.pendingBlocks.first()).isEqualTo(block)
     }
 
     @Test
     fun handleApproveMessageShouldPrepareApproveMessage() {
         val publicKey = "037aa4d9495e30b6b30b94a30f5a573a0f2b365c25eda2d425093b6cf7b826fbd4"
         val delegate = Delegate(publicKey, "address", 1)
-        val genesisBlock = GenesisBlock(
-            1L,
-            "previousHash",
-            2L,
-            4L,
-            publicKey,
-            3L,
-            setOf(delegate)
-        )
         val transactions: MutableSet<Transaction> = mutableSetOf(
             TransferTransaction(
                 1L,
@@ -158,7 +146,7 @@ class DefaultPendingBlockHandlerTests : ServiceTests() {
         defaultPendingBlockHandler.handleApproveMessage(message)
 
         verify(networkService, times(3)).broadcast(any(NetworkBlockApproval::class.java))
-        Assertions.assertThat(defaultPendingBlockHandler.prepareVotes.get(message.publicKey)).isEqualTo(delegate)
+        assertThat(defaultPendingBlockHandler.prepareVotes.get(message.publicKey)).isEqualTo(delegate)
     }
 
     @Test
@@ -177,67 +165,40 @@ class DefaultPendingBlockHandlerTests : ServiceTests() {
 
         defaultPendingBlockHandler.handleApproveMessage(message)
 
-        Assertions.assertThat(defaultPendingBlockHandler.commitVotes.get(message.hash))
+        assertThat(defaultPendingBlockHandler.commitVotes.get(message.hash))
             .isEqualTo(mutableListOf(delegate))
     }
 
-//    @Test
-//    fun handleApproveMessageShouldCommitApproveMessageTwice() {
-//        val publicKey = "037aa4d9495e30b6b30b94a30f5a573a0f2b365c25eda2d425093b6cf7b826fbd4"
-//        val delegate = Delegate(publicKey, "address", 1)
-//        val genesisBlock = GenesisBlock(
-//            1L,
-//            "previousHash",
-//            2L,
-//            publicKey,
-//            3L,
-//            setOf(delegate)
-//        )
-//        val mainBlock = MainBlock(
-//            1L,
-//            "previousHash",
-//            2L,
-//            publicKey,
-//            mutableSetOf(
-//                TransferTransaction(
-//                    1L,
-//                    2L,
-//                    3L,
-//                    "recipientAddress",
-//                    "senderAddress",
-//                    "senderPublicKey",
-//                    "senderSignature",
-//                    "hash",
-//                    null
-//                )
-//            )
-//        )
-//        val message = NetworkBlockApprovalMessage(
-//            ObserverStage.COMMIT.value,
-//            2L,
-//            "2a897fecaaaddcd924a9f562be1cdacf0c7cf3370d1d13c3209f0d05be6bd26f",
-//            publicKey,
-//            "MEUCIDVXCqqrcMuGtHW/m2HEiDM9vcG0wXKA6I0zswsNdtVyAiEA2/orRDpuz0Gp6OI9XsrgW4K8jfFazHbCwqiHq7J1+S8="
-//        )
-//
-//        val privateKey = "529719453390370201f3f0efeeffe4c3a288f39b2e140a3f6074c8d3fc0021e6"
-//        mainBlock.sign(ByteUtils.fromHexString(privateKey))
-//
-//        given(keyHolder.getPrivateKey()).willReturn(
-//            ByteUtils.fromHexString(privateKey))
-//        given(keyHolder.getPublicKey()).willReturn(
-//            ByteUtils.fromHexString(publicKey))
-//        given(timeSlotHelper.getSlotNumber(mainBlock.timestamp)).willReturn(2L)
-//        given(timeSlotHelper.getCurrentSlotOwner()).willReturn(delegate)
-//        given(mainBlockService.isValid(mainBlock)).willReturn(true)
-//        given(genesisBlockService.getLast()).willReturn(genesisBlock)
-//        defaultPendingBlockHandler.addBlock(mainBlock)
-//
-//        defaultPendingBlockHandler.handleApproveMessage(message)
-//        defaultPendingBlockHandler.handleApproveMessage(message)
-//
-//        Assertions.assertThat(defaultPendingBlockHandler.commitVotes.get(message.hash))
-//            .isEqualTo(mutableListOf(delegate))
-//    }
+    @Test
+    fun handleApproveMessageShouldCommitApproveTwoMessages() {
+        addBlockShouldAddMainBlockAndBroadcast()
+        val publicKey = "037aa4d9495e30b6b30b94a30f5a573a0f2b365c25eda2d425093b6cf7b826fbd4"
+        val publicKey2 = "020bf4f11983fca4a99b0d7b18fbffa02462c36126757e598e9beaa33a275f0948"
+        val delegate = Delegate(publicKey, "address", 1)
+        val delegate2 = Delegate(publicKey2, "address2", 2)
+        val message = NetworkBlockApproval(
+            BlockApprovalStage.COMMIT.value,
+            2L,
+            "2b9fa527078f6c5d8b48cbee453e138f6a3a54f9ef1da57b7b464bb17a4d7a72",
+            publicKey,
+            "MEUCIQDkR3bVd0CSPjKMNwPbBfdvp9elSAHri5bptmEfH8k2xwIgQZ45FZodbc43khzM52guR5iXkI1zxDa2hhGk8sQq+gM="
+        )
+        val message2 = NetworkBlockApproval(
+            BlockApprovalStage.COMMIT.value,
+            2L,
+            "2b9fa527078f6c5d8b48cbee453e138f6a3a54f9ef1da57b7b464bb17a4d7a72",
+            publicKey2,
+            "MEUCIQCFYDosVmdgjhSn59dciBBWoyXcc8/m8z2MVuBFbSu31gIgCuz3/9eufmCmBL8tX5cHgLCnKIS3W6vCCVQNo8QWd+0="
+        )
+
+        given(epochService.getDelegates()).willReturn(setOf(delegate, delegate2))
+
+        defaultPendingBlockHandler.handleApproveMessage(message)
+        defaultPendingBlockHandler.handleApproveMessage(message2)
+
+        assertThat(defaultPendingBlockHandler.pendingBlocks.size).isEqualTo(1)
+        verify(networkService, times(2)).broadcast(any(NetworkBlockApproval::class.java))
+        verify(mainBlockService, times(1)).save(any(MainBlock::class.java))
+    }
 
 }
