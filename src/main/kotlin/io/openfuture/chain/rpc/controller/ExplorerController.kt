3d7eb01851e232480b2826936849965d9f582224
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-08-07 17:43:09 +0300

    Added epoch info to explorer info entity

--- src/main/kotlin/io/openfuture/chain/rpc/controller/ExplorerController.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/controller/ExplorerController.kt
@@ -1,35 +1,34 @@
 package io.openfuture.chain.rpc.controller
 
+import io.openfuture.chain.consensus.service.EpochService
 import io.openfuture.chain.core.service.BlockService
-import io.openfuture.chain.core.service.DelegateService
 import io.openfuture.chain.core.service.TransactionService
-import io.openfuture.chain.rpc.domain.explorer.ExplorerDto
+import io.openfuture.chain.rpc.domain.explorer.ExplorerResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 @RequestMapping("/rpc/explorer/info")
 class ExplorerController(
     private val transactionService: TransactionService,
     private val blockService: BlockService,
-    private val delegateService: DelegateService
+    private val epochService: EpochService
 ) {
 
     @GetMapping
-    fun getExplorerInfo(): ExplorerDto {
+    fun getExplorerInfo(): ExplorerResponse {
         val blocksCount = blockService.getCount()
         val blocksSpeed = 1.0
         val transactionsCount = transactionService.getCount()
         val transactionsSpeed = 1.0
         val nodesCount = 0L//networkService.nodesCount()
-        val epochNumber = 0L//blockService.epochNumber()
-        val epochDate = Date()//blockService.epochDate()
-        val delegatesCount = 0L//delegateService.count()
+        val epochNumber = epochService.getEpochIndex()
+        val epochDate = epochService.getEpochStart()
+        val delegatesCount = epochService.getDelegates().size
 
-        return ExplorerDto(nodesCount, blocksCount, transactionsCount, blocksSpeed, transactionsSpeed, epochNumber,
-            epochDate, delegatesCount)
+        return ExplorerResponse(nodesCount, blocksCount, transactionsCount, blocksSpeed, transactionsSpeed, epochNumber,
+            epochDate, delegatesCount.toByte())
     }
 
 }
--- src/main/kotlin/io/openfuture/chain/rpc/domain/explorer/ExplorerResponse.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/domain/explorer/ExplorerResponse.kt
@@ -1,14 +1,12 @@
 package io.openfuture.chain.rpc.domain.explorer
 
-import java.util.*
-
-class ExplorerDto(
+class ExplorerResponse(
     var nodesCount: Long,
     var blocksCount: Long,
     var transactionsCount: Long,
     var blocksSpeed: Double,
     var transactionsSpeed: Double,
     var currectEpochNumber: Long,
-    var currectEpochDate: Date,
-    var delegatesCount: Long
+    var currectEpochDate: Long,
+    var delegatesCount: Byte
 )
