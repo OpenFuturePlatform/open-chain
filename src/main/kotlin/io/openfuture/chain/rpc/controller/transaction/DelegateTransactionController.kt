31160de0665c4200d66b82747dad418a39f075e8
pgomza <pavel.gomza@zensoft.io>
2018-08-07 18:58:44 +0300

    OP-227: update balance by block reward

--- src/main/kotlin/io/openfuture/chain/rpc/controller/transaction/DelegateTransactionController.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/controller/transaction/DelegateTransactionController.kt
@@ -1,19 +1,21 @@
 package io.openfuture.chain.rpc.controller.transaction
 
 import io.openfuture.chain.core.service.DelegateTransactionService
-import io.openfuture.chain.core.service.GenesisBlockService
+import io.openfuture.chain.crypto.util.SignatureUtils
 import io.openfuture.chain.rpc.domain.transaction.request.delegate.DelegateTransactionHashRequest
 import io.openfuture.chain.rpc.domain.transaction.request.delegate.DelegateTransactionRequest
 import io.openfuture.chain.rpc.domain.transaction.response.DelegateTransactionResponse
-import org.springframework.web.bind.annotation.*
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/rpc/transactions/delegates")
 class DelegateTransactionController(
-    private val transactionService: DelegateTransactionService,
-    private val blockService: GenesisBlockService
-) {
+    private val transactionService: DelegateTransactionService) {
 
     @PostMapping("/doGenerateHash")
     fun getHash(@Valid @RequestBody request: DelegateTransactionHashRequest): String {
@@ -24,12 +26,6 @@ class DelegateTransactionController(
     fun add(@Valid @RequestBody request: DelegateTransactionRequest): DelegateTransactionResponse {
         val tx = transactionService.add(request)
         return DelegateTransactionResponse(tx)
-    }
-
-    @GetMapping
-    fun create() {
-        val create = blockService.create()
-        blockService.add(create)
     }
 
 }
