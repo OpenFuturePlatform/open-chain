ea3101bf174a799ba6a3a25a7884a6dfa4e92084
anna.shunko.by <anna.shunko@zensoft.io>
2018-08-09 12:17:36 +0300

    . MainController mapping improved

--- src/main/kotlin/io/openfuture/chain/rpc/controller/base/MainController.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/controller/base/MainController.kt
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 class MainController {
 
-    @RequestMapping(value = ["/", "/login", "/new", "/new/**", "/upload", "/private-key", "/wallet", "/wallet/**"])
+    @RequestMapping(value = ["/", "/login", "/upload", "/private-key",
+        "/new", "/new/restore", "/new/seed-phrase", "/new/keys", "/new/create", "/new/complete",
+        "/wallet", "/wallet/transactions", "/wallet/delegates/vote", "/wallet/delegates/become-a-delegate"])
     fun frontend(): String = "frontend"
 
 }
