e49affb685a88221b32186ed5941840f69be9111
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-08-14 16:22:22 +0300

    Changed explorer info url name

--- src/main/kotlin/io/openfuture/chain/rpc/controller/ExplorerController.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/controller/ExplorerController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/rpc/explorer")
+@RequestMapping("/rpc/explorer/info")
 class ExplorerController(
     private val blockService: BlockService,
     private val epochService: EpochService,
