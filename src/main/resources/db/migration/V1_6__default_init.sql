6528e8175b403e4892692120c3f94472856c21f7
pgomza <pavel.gomza@zensoft.io>
2018-08-23 12:24:18 +0300

    reward: review

--- src/main/resources/db/migration/V1_6__default_init.sql
+++ src/main/resources/db/migration/V1_6__default_init.sql
@@ -1,14 +1,6 @@
-INSERT INTO blocks (id, TIMESTAMP, height, previous_hash, hash, signature, public_key)
-VALUES (1,
-        1532345018021,
-        1,
-        '',
-        '838c84179c7e644cdf2ff0af3055ed45c6f43e0bd7634f8bd6ae7d088b1aaf0a',
-        'MEUCIQCLeQuqCrDd8nmS037ZfmQNtpUf/AsfQilmK7CcNNIi7QIgKNdhszih/PezHW52v4/tdsZxaLovJzDnLvUy98tnsgg=',
-        '038bbbeeb867b999991cd5b146b392ba2fe44ea69d1cc7208e32190184b13aaf1b');
+INSERT INTO blocks(id, TIMESTAMP, height, previous_hash, reward, hash, signature, public_key)
+VALUES (1 , 1532345018021 , 1 , '' , 0 , '838c84179c7e644cdf2ff0af3055ed45c6f43e0bd7634f8bd6ae7d088b1aaf0a',
+         'MEUCIQCLeQuqCrDd8nmS037ZfmQNtpUf/AsfQilmK7CcNNIi7QIgKNdhszih/PezHW52v4/tdsZxaLovJzDnLvUy98tnsgg=',
+         '038bbbeeb867b999991cd5b146b392ba2fe44ea69d1cc7208e32190184b13aaf1b');
 
-INSERT INTO genesis_blocks (id, epoch_index)
-VALUES (1, 1);
-
-insert into wallets (id, address, balance)
-values (1, '0x0000000000000000000000000000000000000000', 10000000000000000);
+INSERT INTO genesis_blocks(id, epoch_index) VALUES (1 , 1);
