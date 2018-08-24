f5489155b852a3a220db87ea80df752f5c03c923
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-08-24 14:21:35 +0300

    Changed DelegateController getAllActive delegates logic

--- src/main/kotlin/io/openfuture/chain/rpc/controller/DelegateController.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/controller/DelegateController.kt
@@ -6,10 +6,11 @@ import io.openfuture.chain.core.service.GenesisBlockService
 import io.openfuture.chain.rpc.domain.DelegateResponse
 import io.openfuture.chain.rpc.domain.base.PageRequest
 import io.openfuture.chain.rpc.domain.base.PageResponse
+import org.springframework.data.domain.PageImpl
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.stream.Collectors.toList
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/rpc/delegates")
@@ -18,15 +19,21 @@ class DelegateController(
     private val genesisBlockService: GenesisBlockService
 ) {
 
-    @GetMapping("/active")
-    fun getAllActive(request: PageRequest): List<DelegateResponse> {
-        val activeDelegates = genesisBlockService.getLast().payload.activeDelegates.map { DelegateResponse(it) }
-
-        return activeDelegates.stream().skip(request.offset).limit(request.getLimit().toLong()).collect(toList())
+    @GetMapping
+    fun getAll(request: PageRequest): PageResponse<DelegateResponse> {
+        val map = delegateService.getAll(request).map { DelegateResponse(it) }
+        return PageResponse(map)
     }
 
-    @GetMapping
-    fun getAll(request: PageRequest): PageResponse<DelegateResponse> =
-        PageResponse(delegateService.getAll(request).map { DelegateResponse(it) })
+    @GetMapping("/active")
+    fun getAllActive(request: PageRequest): PageResponse<DelegateResponse> {
+        val activeDelegates = genesisBlockService.getLast().payload.activeDelegates.map { DelegateResponse(it) }
+        val pageActiveDelegate = activeDelegates.stream()
+            .skip(request.offset)
+            .limit(request.getLimit().toLong())
+            .collect(Collectors.toList())
+
+        return PageResponse(PageImpl(pageActiveDelegate, request, activeDelegates.size.toLong()))
+    }
 
 }
