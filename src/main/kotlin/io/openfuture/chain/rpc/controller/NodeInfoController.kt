d85244c121ed819d10c090a28a7990b1f46a916e
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-08-29 11:40:14 +0300

    Added node info endpoint

--- src/main/kotlin/io/openfuture/chain/rpc/controller/NodeInfoController.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/controller/NodeInfoController.kt
@@ -1,8 +1,13 @@
 package io.openfuture.chain.rpc.controller
 
+import io.openfuture.chain.core.component.NodeKeyHolder
 import io.openfuture.chain.core.model.node.HardwareInfo
 import io.openfuture.chain.core.service.HardwareInfoService
 import io.openfuture.chain.network.component.node.NodeClock
+import io.openfuture.chain.network.property.NodeProperties
+import io.openfuture.chain.rpc.domain.NodeInfoResponse
+import org.apache.commons.lang3.StringUtils
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils
 import org.springframework.context.ApplicationContext
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,9 +20,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/rpc/info")
 class NodeInfoController(
     private val nodeClock: NodeClock,
+    private val properties: NodeProperties,
     private val context: ApplicationContext,
+    private val nodeKeyHolder: NodeKeyHolder,
     private val hardwareInfoService: HardwareInfoService
 ) {
+
+    @GetMapping
+    fun getInfo(): NodeInfoResponse = NodeInfoResponse(nodeKeyHolder.getPublicKey(),
+        nodeKeyHolder.getUid(ByteUtils.fromHexString(nodeKeyHolder.getPublicKey())), StringUtils.EMPTY, properties.port!!)
 
     @GetMapping("/getVersion")
     fun getVersion() { }
