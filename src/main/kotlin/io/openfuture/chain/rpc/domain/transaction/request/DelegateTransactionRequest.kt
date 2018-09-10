de1650e274a75bdc889f9c88e185924d361f093f
anna.shunko.by <anna.shunko@zensoft.io>
2018-09-10 12:45:49 +0300

    Address validation added to transaction requests

--- src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/request/DelegateTransactionRequest.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/request/DelegateTransactionRequest.kt
@@ -1,12 +1,13 @@
 package io.openfuture.chain.rpc.domain.transaction.request
 
+import io.openfuture.chain.crypto.annotation.AddressChecksum
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 data class DelegateTransactionRequest(
     @field:NotNull var timestamp: Long? = null,
     @field:NotNull var fee: Long? = null,
-    @field:NotBlank var senderAddress: String? = null,
+    @field:NotBlank @field:AddressChecksum var senderAddress: String? = null,
     @field:NotNull var amount: Long? = null,
     @field:NotBlank var nodeId: String? = null,
     @field:NotBlank var nodeKey: String? = null,
