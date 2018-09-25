6f2a152e4f3a7288c5505ee6d491502d5e16cad0
anna.shunko.by <anna.shunko@zensoft.io>
2018-09-25 10:34:44 +0300

    Unused classes removed

--- src/main/kotlin/io/openfuture/chain/core/model/domain/block/TransactionSelectionRequest.kt
+++
@@ -1,7 +0,0 @@
-package io.openfuture.chain.core.model.domain.block
-
-data class TransactionSelectionRequest(
-    var voteTransactionsCount: Int = 0,
-    var delegateTransactionsCount: Int = 0,
-    var transferTransactionsCount: Int = 0
-)
