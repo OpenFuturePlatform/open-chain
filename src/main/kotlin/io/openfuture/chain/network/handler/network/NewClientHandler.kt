673d992f6990736f752d4bf207592d3b62efc04e
yury.shakhau.by <yury.shakhau@zensoft.io>
2018-11-15 17:07:04 +0300

    Broadcast nodeInfo within the entire the network is added

--- src/main/kotlin/io/openfuture/chain/network/handler/network/NewClientHandler.kt
+++ src/main/kotlin/io/openfuture/chain/network/handler/network/NewClientHandler.kt
@@ -3,6 +3,7 @@ package io.openfuture.chain.network.handler.network
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
+import io.openfuture.chain.network.component.ChannelsHolder
 import io.openfuture.chain.network.component.ExplorerAddressesHolder
 import io.openfuture.chain.network.message.network.NewClient
 import org.springframework.stereotype.Component
@@ -10,11 +11,16 @@ import org.springframework.stereotype.Component
 @Component
 @Sharable
 class NewClientHandler(
-    private val explorerAddressesHolder: ExplorerAddressesHolder
+    private val explorerAddressesHolder: ExplorerAddressesHolder,
+    private val channelsHolder: ChannelsHolder
 ) : SimpleChannelInboundHandler<NewClient>() {
 
     override fun channelRead0(ctx: ChannelHandlerContext, msg: NewClient) {
-        explorerAddressesHolder.addNodeInfo(msg.nodeInfo)
+        val nodeInfo = msg.nodeInfo
+        if (!explorerAddressesHolder.hasNodeInfo(nodeInfo)) {
+            explorerAddressesHolder.addNodeInfo(nodeInfo)
+            channelsHolder.broadcast(msg)
+        }
     }
 
 }
