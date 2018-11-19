b4b88d2073179d5e15271c38fc99a2bcd487f0ed
anna.shunko <anna.shunko@zensoft.io>
2018-11-19 16:34:29 +0300

    status handlers combined

--- src/main/kotlin/io/openfuture/chain/network/handler/sync/SyncChainStatusHandler.kt
+++ src/main/kotlin/io/openfuture/chain/network/handler/sync/SyncChainStatusHandler.kt
@@ -5,17 +5,20 @@ import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
 import io.openfuture.chain.core.sync.SyncManager
 import io.openfuture.chain.core.sync.SyncStatus.SYNCHRONIZED
+import io.openfuture.chain.network.component.time.ClockSynchronizer
 import io.openfuture.chain.network.serialization.Serializable
 import org.springframework.stereotype.Component
 
 @Component
 @Sharable
-class SyncChainStatusHandler(
-    private val syncManager: SyncManager
+class SyncStatusHandler(
+    private val syncManager: SyncManager,
+    private val clockSynchronizer: ClockSynchronizer
+
 ) : SimpleChannelInboundHandler<Serializable>() {
 
     override fun channelRead0(ctx: ChannelHandlerContext, msg: Serializable) {
-        if (SYNCHRONIZED == syncManager.getStatus()) {
+        if (SYNCHRONIZED == syncManager.getStatus() && SYNCHRONIZED == clockSynchronizer.getStatus()) {
             ctx.fireChannelRead(msg)
         }
     }
