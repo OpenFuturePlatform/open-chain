679cbe5dc6f925394bb2e48137edb589729f14a2
anna.shunko <anna.shunko@zensoft.io>
2018-12-03 12:50:57 +0300

    Default address value

--- src/main/kotlin/io/openfuture/chain/smartcontract/core/model/SmartContract.kt
+++ src/main/kotlin/io/openfuture/chain/smartcontract/core/model/SmartContract.kt
@@ -3,7 +3,7 @@ package io.openfuture.chain.smartcontract.core.model
 import io.openfuture.chain.smartcontract.core.exception.RequiredException
 import io.openfuture.chain.smartcontract.core.utils.AddressUtils
 
-abstract class SmartContract(ownerAddress: String) {
+abstract class SmartContract(ownerAddress: String = "") {
 
     protected val owner: Address = Address(ownerAddress)
     //TODO calculate from owner address (Keccak256 hash of sender address and nonce)
