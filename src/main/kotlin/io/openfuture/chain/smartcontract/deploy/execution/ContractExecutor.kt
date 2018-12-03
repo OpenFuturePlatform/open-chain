030807c1a810311daed383569a26b4b84bd96d61
anna.shunko <anna.shunko@zensoft.io>
2018-12-03 17:35:19 +0300

    Contract executor added

---
+++ src/main/kotlin/io/openfuture/chain/smartcontract/deploy/execution/ContractExecutor.kt
@@ -0,0 +1,44 @@
+package io.test.io.openfuture.chain.smartcontract.deploy.service
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+class ContractExecutor {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(ContractExecutor::class.java)
+    }
+
+    private val pool = Executors.newFixedThreadPool(4)
+
+
+    fun run(runnableClass: String, method: String, vararg input: Any): Result {
+        var output: Any? = null
+        var exception: Throwable? = null
+        val completionLatch = CountDownLatch(1)
+        pool.execute {
+            try {
+                log.debug("Execution started for $runnableClass - $method")
+                val clazz = Class.forName(runnableClass)
+                val instance = clazz.newInstance()
+                output = clazz.getDeclaredMethod(method, *input.map { it.javaClass }.toTypedArray()).invoke(instance, *input)
+            } catch (ex: Throwable) {
+                log.debug("Error while executing ($runnableClass - $method): ${ex.message}")
+                exception = ex
+            }
+            log.debug("Execution completed for $runnableClass - $method")
+            completionLatch.countDown()
+        }
+
+        completionLatch.await()
+        return Result(runnableClass, output, exception)
+    }
+
+    data class Result(
+        val identifier: String,
+        val output: Any?,
+        val exception: Throwable?
+    )
+}
