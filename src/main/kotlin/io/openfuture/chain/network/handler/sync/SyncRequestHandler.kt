5f2976fb164bba4cc39bd544c0015a0ebf8f7190
Pavel Evleev <pavel.evleev@zensoft.io>
2019-01-02 15:15:47 +0300

    handlers are changed

--- src/main/kotlin/io/openfuture/chain/network/handler/sync/SyncRequestHandler.kt
+++ src/main/kotlin/io/openfuture/chain/network/handler/sync/SyncRequestHandler.kt
@@ -3,12 +3,9 @@ package io.openfuture.chain.network.handler.sync
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
-import io.openfuture.chain.core.service.BlockService
-import io.openfuture.chain.network.component.time.Clock
-import io.openfuture.chain.network.message.sync.SyncBlockDto
+import io.openfuture.chain.core.service.GenesisBlockService
 import io.openfuture.chain.network.message.sync.SyncRequestMessage
 import io.openfuture.chain.network.message.sync.SyncResponseMessage
-import io.openfuture.chain.network.property.NodeProperties
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -16,9 +13,7 @@ import org.springframework.stereotype.Component
 @Component
 @Sharable
 class SyncRequestHandler(
-    private val clock: Clock,
-    private val blockService: BlockService,
-    private val properties: NodeProperties
+    private val genesisBlockService: GenesisBlockService
 ) : SimpleChannelInboundHandler<SyncRequestMessage>() {
 
     companion object {
@@ -27,19 +22,9 @@ class SyncRequestHandler(
 
 
     override fun channelRead0(ctx: ChannelHandlerContext, msg: SyncRequestMessage) {
-        if (properties.expiry!! < clock.currentTimeMillis() - msg.timestamp) {
-            log.debug("Expired sync request")
-            return
-        }
-
-        if (!blockService.isExists(msg.lastBlockHash, msg.lastBlockHeight)) {
-            log.debug("Block request NOT EXIST")
-            return
-        }
-
-        val lastBlocks = blockService.getCarcassForBlockSync(msg.lastBlockHash).map { SyncBlockDto(it.height, it.hash) }
-        ctx.writeAndFlush(SyncResponseMessage(clock.currentTimeMillis(), lastBlocks))
-        log.debug("REQUEST from ${ctx.channel().remoteAddress()} handled: send ${lastBlocks.size}-[${lastBlocks.map { it.height }}]")
+        val lastGenesisBlock = genesisBlockService.getLast()
+        ctx.writeAndFlush(SyncResponseMessage(lastGenesisBlock.toMessage()))
+        log.debug("REQUEST from ${ctx.channel().remoteAddress()} handled: send ${lastGenesisBlock.hash}")
     }
 
 }
