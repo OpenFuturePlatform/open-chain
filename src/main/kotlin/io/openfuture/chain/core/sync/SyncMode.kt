29880dc0e7d7468e22d7096bf146808717d862ae
Nikita Eliseenko <nikita.eliseenko@zensoft.io>
2019-01-15 12:25:44 +0300

    logic for sync mode enum

--- src/main/kotlin/io/openfuture/chain/core/sync/SyncMode.kt
+++ src/main/kotlin/io/openfuture/chain/core/sync/SyncMode.kt
@@ -1,6 +1,22 @@
 package io.openfuture.chain.core.sync
 
+
 enum class SyncMode {
     LIGHT,
-    FULL
+    FULL;
+
+    companion object {
+
+        fun ofByte(id: Byte): SyncMode = values()[id.toInt()]
+
+        fun toByte(name: SyncMode): Byte {
+            for (i in 0..values().size) {
+                if (values()[i] == name) {
+                    return i.toByte()
+                }
+            }
+            throw IllegalArgumentException("Sync mode by name: $name not found")
+        }
+    }
 }
+
