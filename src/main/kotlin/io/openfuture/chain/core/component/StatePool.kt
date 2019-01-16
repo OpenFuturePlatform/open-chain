a66f428f6be36b153722b1f225aa4c6899e1b279
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-01-16 11:07:25 +0300

    Added state pool

---
+++ src/main/kotlin/io/openfuture/chain/core/component/StatePool.kt
@@ -0,0 +1,23 @@
+package io.openfuture.chain.core.component
+
+import io.openfuture.chain.core.model.entity.State
+import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+class StatePool {
+
+    private val pool: MutableMap<String, State> = ConcurrentHashMap()
+
+
+    fun getPool(): Map<String, State> = pool
+
+    fun getByAddress(address: String): State? = pool[address]
+
+    fun update(state: State): State? = pool.put(state.address, state)
+
+    fun clear() {
+        pool.clear()
+    }
+
+}
