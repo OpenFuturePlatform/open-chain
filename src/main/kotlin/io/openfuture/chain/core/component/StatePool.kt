e90e3655d2ce1fe91491f3a246a1e2ef3b63e029
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-01-17 17:20:56 +0300

    Changed state pool

--- src/main/kotlin/io/openfuture/chain/core/component/StatePool.kt
+++ src/main/kotlin/io/openfuture/chain/core/component/StatePool.kt
@@ -1,6 +1,6 @@
 package io.openfuture.chain.core.component
 
-import io.openfuture.chain.core.model.entity.state.State
+import io.openfuture.chain.network.message.core.StateMessage
 import org.springframework.stereotype.Component
 import java.io.Closeable
 import java.util.concurrent.ConcurrentHashMap
@@ -8,14 +8,14 @@ import java.util.concurrent.ConcurrentHashMap
 @Component
 class StatePool : Closeable {
 
-    private val pool: MutableMap<String, State> = ConcurrentHashMap()
+    private val pool: MutableMap<String, StateMessage> = ConcurrentHashMap()
 
 
-    fun getPool(): Map<String, State> = pool
+    fun getPool(): Map<String, StateMessage> = pool
 
-    fun get(address: String): State? = pool[address]
+    fun get(address: String): StateMessage? = pool[address]
 
-    fun update(state: State): State? = pool.put(state.address, state)
+    fun update(state: StateMessage): StateMessage? = pool.put(state.address, state)
 
     fun clear() {
         pool.clear()
