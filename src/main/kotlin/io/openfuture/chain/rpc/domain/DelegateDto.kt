8e98aec706ad0b9443cf84938b1f8dc49eb3e503
anna.shunko <anna.shunko@zensoft.io>
2019-01-21 11:48:49 +0300

    DelegateDto moved to domain package

---
+++ src/main/kotlin/io/openfuture/chain/rpc/domain/DelegateDto.kt
@@ -0,0 +1,12 @@
+package io.openfuture.chain.rpc.domain
+
+class DelegateDto(
+    var publicKey: String = "",
+    var nodeId: String = "",
+    var address: String = "",
+    var host: String = "",
+    var port: Int = 0,
+    var registrationDate: Long = 0L,
+    var rating: Long = 0L,
+    var votesCount: Long = 0L
+)
