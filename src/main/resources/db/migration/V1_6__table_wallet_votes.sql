1efaf84e0f2902aa2fefb98b7e64a55115d547a6
anna.shunko <anna.shunko@zensoft.io>
2019-01-21 15:02:38 +0300

    Migration improved

--- src/main/resources/db/migration/V1_6__table_wallet_votes.sql
+++ src/main/resources/db/migration/V1_6__table_wallet_votes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE wallet_votes (
   address VARCHAR NOT NULL,
-  delegate_key VARCHAR NOT NULL REFERENCES delegates(node_id),
+  delegate_key VARCHAR NOT NULL REFERENCES delegates(public_key),
   PRIMARY KEY (address, delegate_key)
 );
