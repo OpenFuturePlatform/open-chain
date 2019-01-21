20f6f81771cc69fe393829eb4b1634f5efd31638
Nikita Eliseenko <nikita.eliseenko@zensoft.io>
2019-01-21 18:16:46 +0300

    hash indexes is added for Delegates

--- src/main/resources/db/migration/V1_4__table_delegates.sql
+++ src/main/resources/db/migration/V1_4__table_delegates.sql
@@ -8,10 +8,10 @@ CREATE TABLE delegates (
   registration_date BIGINT  NOT NULL
 );
 --
-CREATE INDEX delegates_public_key
+CREATE HASH INDEX delegates_public_key
   ON delegates (public_key);
 --
-CREATE INDEX delegates_node_id
+CREATE HASH INDEX delegates_node_id
   ON delegates (node_id);
 --
 CREATE TABLE delegate2genesis (
