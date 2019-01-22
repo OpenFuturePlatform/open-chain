8f801781440a012205178e3c0be6dea1b53680d7
anna.shunko <anna.shunko@zensoft.io>
2019-01-22 15:59:28 +0300

    transaction controller rolled back

--- src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/request/DelegateTransactionRequest.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/request/DelegateTransactionRequest.kt
@@ -1,12 +1,6 @@
 package io.openfuture.chain.rpc.domain.transaction.request
 
-import io.openfuture.chain.core.model.entity.transaction.TransactionHeader
-import io.openfuture.chain.core.model.entity.transaction.payload.DelegateTransactionPayload
 import io.openfuture.chain.crypto.annotation.AddressChecksum
-import io.openfuture.chain.crypto.util.HashUtils
-import io.openfuture.chain.crypto.util.SignatureUtils
-import org.bouncycastle.pqc.math.linearalgebra.ByteUtils
-import java.nio.ByteBuffer
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
@@ -22,20 +16,4 @@ data class DelegateTransactionRequest(
     @field:NotBlank var hash: String? = null,
     @field:NotBlank var senderSignature: String? = null,
     @field:NotBlank var senderPublicKey: String? = null
-) {
-
-    fun createHash(): String {
-        val header = TransactionHeader(timestamp!!, fee!!, senderAddress!!)
-        val payload = DelegateTransactionPayload(nodeKey!!, amount!!)
-        val bytes = ByteBuffer.allocate(header.getBytes().size + payload.getBytes().size)
-            .put(header.getBytes())
-            .put(payload.getBytes())
-            .array()
-
-        return ByteUtils.toHexString(HashUtils.doubleSha256(bytes))
-    }
-
-    fun sign(privateKey: String): String =
-        SignatureUtils.sign(ByteUtils.fromHexString(hash), ByteUtils.fromHexString(privateKey))
-
-}
+)
