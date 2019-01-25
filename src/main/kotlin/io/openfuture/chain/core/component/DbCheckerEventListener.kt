7105a31be9e5bb23e3d2c3ee4fd48c2f5e835400
yury.shakhau.by <yury.shakhau@zensoft.io>
2019-01-25 14:34:55 +0300

    DBCheckerListener created

---
+++ src/main/kotlin/io/openfuture/chain/core/component/DbCheckerEventListener.kt
@@ -0,0 +1,112 @@
+package io.openfuture.chain.core.component
+
+import io.openfuture.chain.consensus.property.ConsensusProperties
+import io.openfuture.chain.core.model.entity.block.Block
+import io.openfuture.chain.core.model.entity.block.MainBlock
+import io.openfuture.chain.core.model.entity.block.payload.MainBlockPayload
+import io.openfuture.chain.core.service.BlockService
+import io.openfuture.chain.core.service.GenesisBlockService
+import org.springframework.boot.autoconfigure.jdbc.DataSourceSchemaCreatedEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.stereotype.Component
+
+@Component
+class DBCheckerListener(
+    private val consensusProperties: ConsensusProperties,
+    private val genesisBlockService: GenesisBlockService,
+    private val blockService: BlockService,
+    private val syncCursor: SyncCursor
+) : ApplicationListener<DataSourceSchemaCreatedEvent> {
+
+    private var cursorFlag: Boolean = true
+
+    override fun onApplicationEvent(event: DataSourceSchemaCreatedEvent) {
+        if (!isValidDb()) {
+            val heightFrom = syncCursor.fullCursor.height
+            val heightTo = blockService.getLast().height
+            val heightsToDelete = ArrayList<Long>()
+            for (i in heightFrom..heightTo) {
+                heightsToDelete.add(i)
+            }
+            blockService.deleteByHeightIn(heightsToDelete)
+        }
+    }
+
+    private fun isValidDb(): Boolean {
+        val epochHeight = consensusProperties.epochHeight!!
+        syncCursor.fullCursor = genesisBlockService.getByEpochIndex(1L)!!
+        var indexFrom = syncCursor.fullCursor.height
+        var indexTo = indexFrom + epochHeight
+        var block: Block? = null
+        do {
+            val blocks = blockService.findAllByHeightBetween(indexFrom, indexTo)
+            indexFrom += epochHeight + 1
+            indexTo += epochHeight + 1
+
+            val blocksIterator = blocks.iterator()
+            if (null == block) {
+                block = blocksIterator.next()
+            }
+            while (blocksIterator.hasNext()) {
+                val nextBlock = blocksIterator.next()
+
+                if (!isValidBlocks(block!!, nextBlock)) {
+                    return false
+                }
+
+                block = nextBlock
+            }
+        } while (!blocks.isEmpty())
+        return isValidBlock(block!!)
+    }
+
+    private fun isValidBlocks(block: Block, nextBlock: Block): Boolean {
+        if (!isValidBlocksHashes(block, nextBlock)) {
+            return false
+        }
+        if (!isValidBlock(block)) {
+            return false
+        }
+        return true
+    }
+
+    private fun isValidBlock(block: Block): Boolean {
+        if (!isValidBlockState(block)) {
+            return false
+        }
+        if (!isValidTransactions(block)) {
+            cursorFlag = false
+            return false
+        }
+        if (cursorFlag) {
+            syncCursor.fullCursor = block
+        }
+        return true
+    }
+
+    private fun isValidTransactions(block: Block): Boolean {
+        if (block is MainBlock) {
+            val hashes = mutableListOf<String>()
+            hashes.addAll(block.payload.transferTransactions.map { it.footer.hash })
+            hashes.addAll(block.payload.voteTransactions.map { it.footer.hash })
+            hashes.addAll(block.payload.delegateTransactions.map { it.footer.hash })
+            hashes.add(block.payload.rewardTransaction[0].footer.hash)
+
+            if (hashes.isEmpty()) {
+                return true
+            }
+
+            val transactionsMerkleHash = MainBlockPayload.calculateMerkleRoot(hashes)
+            if (block.payload.merkleHash != transactionsMerkleHash) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun isValidBlocksHashes(block: Block, nextBlock: Block): Boolean = (block.hash == nextBlock.previousHash)
+
+    //TODO: must to be implemented
+    private fun isValidBlockState(block: Block): Boolean = true
+
+}
