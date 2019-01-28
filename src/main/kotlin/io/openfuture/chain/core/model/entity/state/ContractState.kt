84800dd2d51ebeb3d86396d8c324398b014d571d
anna.shunko <anna.shunko@zensoft.io>
2019-01-28 17:42:36 +0300

    Contract state added

---
+++ src/main/kotlin/io/openfuture/chain/core/model/entity/state/ContractState.kt
@@ -0,0 +1,27 @@
+package io.openfuture.chain.core.model.entity.state
+
+import io.openfuture.chain.core.model.entity.block.MainBlock
+import io.openfuture.chain.network.message.core.ContractStateMessage
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name = "contract_states")
+class ContractState(
+    address: String,
+    block: MainBlock,
+
+    @Column(name = "storage", nullable = false)
+    val storage: ByteArray
+
+) : State(address, block) {
+
+    companion object {
+        fun of(message: ContractStateMessage, block: MainBlock): ContractState =
+            ContractState(message.address, block, message.storage)
+    }
+
+    override fun toMessage(): ContractStateMessage = ContractStateMessage(address, storage)
+
+}
