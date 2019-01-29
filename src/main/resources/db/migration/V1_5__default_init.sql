820d8895005b4dae884ebb934e32a300c14bd22d
yury.shakhau.by <yury.shakhau@zensoft.io>
2019-01-29 09:56:55 +0300

    First genesis block signature is changed

--- src/main/resources/db/migration/V1_5__default_init.sql
+++ src/main/resources/db/migration/V1_5__default_init.sql
@@ -3,9 +3,9 @@ VALUES (1,
         1532345018021,
         1,
         '',
-        '838c84179c7e644cdf2ff0af3055ed45c6f43e0bd7634f8bd6ae7d088b1aaf0a',
-        'MEUCIQCLeQuqCrDd8nmS037ZfmQNtpUf/AsfQilmK7CcNNIi7QIgKNdhszih/PezHW52v4/tdsZxaLovJzDnLvUy98tnsgg=',
-        '038bbbeeb867b999991cd5b146b392ba2fe44ea69d1cc7208e32190184b13aaf1b');
+        '551c9603385edbdd7917e748d65f7ee1496280f7f491c56a6c907743e68188bb',
+        'MEUCIQD14fppWIIrdNtljuDEpSA33loW5GcPB9Q938A+pQJ31wIgRHq0vE6ogzZvyr62wIxsO42UPKHJxy8C9RIpZnYN3cA=',
+        '02b04aa1832e799503000a6b8da1cdbb737c167fc829472c726a12ad9a4ccf24eb');
 
 INSERT INTO genesis_blocks (id, epoch_index)
 VALUES (1, 1);
