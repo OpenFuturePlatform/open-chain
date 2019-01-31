244e496eae06564ea6ce6284cfb443ff37bf095a
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-01-31 16:22:41 +0300

    Improved smart exceptions

--- src/main/kotlin/io/openfuture/chain/smartcontract/exception/SmartContractException.kt
+++ src/main/kotlin/io/openfuture/chain/smartcontract/exception/SmartContractException.kt
@@ -3,5 +3,3 @@ package io.openfuture.chain.smartcontract.exception
 open class SmartContractException(message: String?) : RuntimeException(message)
 
 class SmartContractClassCastException(message: String) : SmartContractException(message)
-
-class SmartContractValidationException(message: String) : SmartContractException(message)
