ff15c9967c75836bb62a2e6f0bf69a18db3e3728
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-02-01 15:59:38 +0300

    Added sdk

--- src/main/kotlin/io/openfuture/chain/smartcontract/util/TypeExtensions.kt
+++
@@ -1,7 +0,0 @@
-package io.openfuture.chain.smartcontract.util
-
-val String.asPackagePath: String get() = this.replace('/', '.')
-
-val String.asResourcePath: String get() = this.replace('.', '/')
-
-fun Set<String>.toOrRegex(): Regex = this.joinToString(separator = "|") { "(.*$it.*)" }.toRegex()
