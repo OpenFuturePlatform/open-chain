3a29d196707a23d6795bd66f086dc09a155cfa15
anna.shunko <anna.shunko@zensoft.io>
2019-02-04 15:20:20 +0300

    transaction validator added

--- src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/request/TransferTransactionRequest.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/request/TransferTransactionRequest.kt
@@ -1,9 +1,11 @@
 package io.openfuture.chain.rpc.domain.transaction.request
 
 import io.openfuture.chain.crypto.annotation.AddressChecksum
+import io.openfuture.chain.rpc.validation.annotation.TransferTransaction
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
+@TransferTransaction
 data class TransferTransactionRequest(
     @field:NotNull var timestamp: Long? = null,
     @field:NotNull var fee: Long? = null,
