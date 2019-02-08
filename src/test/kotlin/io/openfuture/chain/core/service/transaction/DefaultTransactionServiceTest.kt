5a8d6112e0c6985f08152f05b15c173f55ab4a4d
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-02-08 17:30:02 +0300

    Improved test

--- src/test/kotlin/io/openfuture/chain/core/service/transaction/DefaultTransactionServiceTest.kt
+++ src/test/kotlin/io/openfuture/chain/core/service/transaction/DefaultTransactionServiceTest.kt
@@ -1,10 +1,11 @@
 package io.openfuture.chain.core.service.transaction
 
+import io.openfuture.chain.config.ServiceTests
+import io.openfuture.chain.core.model.entity.state.AccountState
 import io.openfuture.chain.core.repository.TransferTransactionRepository
 import io.openfuture.chain.core.repository.UTransferTransactionRepository
 import io.openfuture.chain.core.service.AccountStateService
 import io.openfuture.chain.core.service.ContractService
-import io.openfuture.chain.network.message.core.AccountStateMessage
 import io.openfuture.chain.network.message.core.TransferTransactionMessage
 import io.openfuture.chain.smartcontract.deploy.calculation.ContractCostCalculator
 import io.openfuture.chain.smartcontract.execution.ContractExecutor
@@ -13,121 +14,109 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.BDDMockito.`when`
-import org.mockito.BDDMockito.anyString
+import org.mockito.BDDMockito.*
 import org.mockito.Mock
-import org.springframework.boot.test.mock.mockito.SpyBean
-import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.util.ReflectionTestUtils
 
-@RunWith(SpringRunner::class)
-class DefaultTransactionServiceTest {
+class DefaultTransactionServiceTest : ServiceTests() {
 
-    @Mock
-    private lateinit var repository: TransferTransactionRepository
-
-    @Mock
-    private lateinit var uRepository: UTransferTransactionRepository
-
-    @Mock
-    private lateinit var contractSevice: ContractService
-
-    @Mock
-    private lateinit var accountStateService: AccountStateService
-
-    @Mock
-    private lateinit var contractExecutor: ContractExecutor
-
-    @SpyBean
-    private lateinit var contractCostCalculator: ContractCostCalculator
+    @Mock private lateinit var repository: TransferTransactionRepository
+    @Mock private lateinit var uRepository: UTransferTransactionRepository
+    @Mock private lateinit var contractService: ContractService
+    @Mock private lateinit var accountStateService: AccountStateService
+    @Mock private lateinit var contractExecutor: ContractExecutor
+    @Mock private lateinit var contractCostCalculator: ContractCostCalculator
 
     private lateinit var transactionService: DefaultTransferTransactionService
-
     private lateinit var transactionMessage: TransferTransactionMessage
 
     private val delegateAddress = "delegateAddress"
-
     private val senderAddress = "senderAddress"
-
-    private var contractCost: Long = 0L
+    private var contractCost: Long = 30L
 
 
     @Before
     fun setUp() {
-        transactionService = DefaultTransferTransactionService(repository, uRepository, contractSevice, contractCostCalculator, contractExecutor)
-        ReflectionTestUtils.setField(transactionService, "accountStateService", accountStateService, AccountStateService::class.java)
+        transactionService = DefaultTransferTransactionService(repository, uRepository, contractService,
+            contractCostCalculator, contractExecutor)
+        ReflectionTestUtils.setField(transactionService, "accountStateService", accountStateService,
+            AccountStateService::class.java)
         val code = this::class.java.getResourceAsStream("/classes/JavaContract.class").readBytes()
         val bytecode = ByteUtils.toHexString(code)
-        contractCost = contractCostCalculator.calculateCost(code)
         transactionMessage = TransferTransactionMessage(121325454, 0, senderAddress, "", "", "", 0, null, bytecode)
 
+        given(contractCostCalculator.calculateCost(code)).willReturn(contractCost)
     }
 
     @Test
-    fun processShouldReturnErrorResultReceipt() {
+    fun processWhenFeeIsNotEnoughShouldReturnErrorResultReceipt() {
         val sendFee = 10L
         val expectedSize = 1
         val expectedError = "Contract is not deployed."
         transactionMessage.fee = sendFee
 
-        `when`(accountStateService.updateBalanceByAddress(senderAddress, -sendFee)).thenReturn(AccountStateMessage(senderAddress, 0))
-        `when`(accountStateService.updateBalanceByAddress(delegateAddress, sendFee)).thenReturn(AccountStateMessage(delegateAddress, 0))
+        given(accountStateService.updateBalanceByAddress(senderAddress, -sendFee))
+            .willReturn(AccountState(senderAddress, 0))
+        given(accountStateService.updateBalanceByAddress(delegateAddress, sendFee))
+            .willReturn(AccountState(delegateAddress, 0))
 
         val result = transactionService.process(transactionMessage, delegateAddress)
-        assertEquals(result.getResults().size, expectedSize)
-        assertEquals(result.getResults().first().error, expectedError)
+
+        assertEquals(expectedSize, result.getResults().size)
+        assertEquals(expectedError, result.getResults().first().error)
     }
 
     @Test
-    fun processShouldReturnTwoResultReceipt() {
+    fun processWhenFeeIsGreaterThanContractCostShouldReturnTwoResultReceipt() {
         val sendFee = 40L
         val expectedSize = 2
         val expectedDelivery = sendFee - contractCost
         transactionMessage.fee = sendFee
 
-        `when`(contractSevice.generateAddress(senderAddress)).thenReturn(senderAddress)
-        `when`(accountStateService.updateBalanceByAddress(senderAddress, -sendFee)).thenReturn(AccountStateMessage(senderAddress, 0))
-        `when`(accountStateService.updateBalanceByAddress(delegateAddress, sendFee)).thenReturn(AccountStateMessage(delegateAddress, 0))
-        `when`(accountStateService.updateStorage(anyString(), anyString())).thenReturn(AccountStateMessage(anyString(), 0))
+        given(contractService.generateAddress(senderAddress)).willReturn(senderAddress)
 
         val result = transactionService.process(transactionMessage, delegateAddress)
-        val expectedSenderReceiptResult = result.getResults().find { it.from == senderAddress }
-        val expectedDelegateReceiptResult = result.getResults().find { it.from == delegateAddress }
+        val actualSenderReceiptResult = result.getResults().find { it.from == senderAddress }
+        val actualDelegateReceiptResult = result.getResults().find { it.from == delegateAddress }
 
-        assertEquals(result.getResults().size, expectedSize)
+        assertEquals(expectedSize, result.getResults().size)
 
-        assertEquals(expectedSenderReceiptResult?.to, delegateAddress)
-        assertEquals(expectedSenderReceiptResult?.amount, sendFee)
-        assertTrue(expectedSenderReceiptResult?.error.isNullOrBlank())
-        assertTrue(expectedSenderReceiptResult?.data.isNullOrBlank())
+        assertEquals(delegateAddress, actualSenderReceiptResult?.to)
+        assertEquals(sendFee, actualSenderReceiptResult?.amount)
+        assertTrue(actualSenderReceiptResult?.error.isNullOrBlank())
+        assertTrue(actualSenderReceiptResult?.data.isNullOrBlank())
 
-        assertEquals(expectedDelegateReceiptResult?.to, senderAddress)
-        assertEquals(expectedDelegateReceiptResult?.amount, expectedDelivery)
-        assertTrue(expectedDelegateReceiptResult?.error.isNullOrBlank())
-        assertTrue(expectedDelegateReceiptResult?.error.isNullOrBlank())
+        assertEquals(senderAddress, actualDelegateReceiptResult?.to)
+        assertEquals(expectedDelivery, actualDelegateReceiptResult?.amount)
+        assertTrue(actualDelegateReceiptResult?.error.isNullOrBlank())
+        assertTrue(actualDelegateReceiptResult?.data.isNullOrBlank())
+
+        verify(accountStateService).updateStorage(anyString(), anyString())
+        verify(accountStateService).updateBalanceByAddress(senderAddress, -contractCost)
+        verify(accountStateService).updateBalanceByAddress(delegateAddress, contractCost)
     }
 
     @Test
-    fun processShouldReturnOneResultReceipt() {
+    fun processWhenFeeIsEqualContractCostShouldReturnOneResultReceipt() {
         val sendFee = 30L
         val expectedSize = 1
         transactionMessage.fee = sendFee
 
-        `when`(contractSevice.generateAddress(senderAddress)).thenReturn(senderAddress)
-        `when`(accountStateService.updateBalanceByAddress(senderAddress, -sendFee)).thenReturn(AccountStateMessage(senderAddress, 0))
-        `when`(accountStateService.updateBalanceByAddress(delegateAddress, sendFee)).thenReturn(AccountStateMessage(delegateAddress, 0))
-        `when`(accountStateService.updateStorage(anyString(), anyString())).thenReturn(AccountStateMessage(anyString(), 0))
+        given(contractService.generateAddress(senderAddress)).willReturn(senderAddress)
 
         val result = transactionService.process(transactionMessage, delegateAddress)
-        val expectedReceiptResult = result.getResults().first()
+        val actualReceiptResult = result.getResults().first()
 
-        assertEquals(result.getResults().size, expectedSize)
-        assertEquals(expectedReceiptResult.from, senderAddress)
-        assertEquals(expectedReceiptResult.to, delegateAddress)
-        assertEquals(expectedReceiptResult.amount, sendFee)
-        assertTrue(expectedReceiptResult.error.isNullOrBlank())
-        assertTrue(expectedReceiptResult.data.isNullOrBlank())
+        assertEquals(expectedSize, result.getResults().size)
+        assertEquals(senderAddress, actualReceiptResult.from)
+        assertEquals(delegateAddress, actualReceiptResult.to)
+        assertEquals(sendFee, actualReceiptResult.amount)
+        assertTrue(actualReceiptResult.error.isNullOrBlank())
+        assertTrue(actualReceiptResult.data.isNullOrBlank())
+
+        verify(accountStateService).updateStorage(anyString(), anyString())
+        verify(accountStateService).updateBalanceByAddress(senderAddress, -contractCost)
+        verify(accountStateService).updateBalanceByAddress(delegateAddress, contractCost)
     }
 
 }
