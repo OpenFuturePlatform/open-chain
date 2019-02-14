2e7fc29c3ed01a9cf1aebe540c461f377734ce25
anna.shunko <anna.shunko@zensoft.io>
2019-02-14 16:08:58 +0300

    Transaction status and receipts added to response

--- src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/response/TransferTransactionResponse.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/domain/transaction/response/TransferTransactionResponse.kt
@@ -1,7 +1,9 @@
 package io.openfuture.chain.rpc.domain.transaction.response
 
+import io.openfuture.chain.core.model.entity.Receipt
 import io.openfuture.chain.core.model.entity.transaction.confirmed.TransferTransaction
 import io.openfuture.chain.core.model.entity.transaction.unconfirmed.UnconfirmedTransferTransaction
+import io.openfuture.chain.rpc.domain.ReceiptResultResponse
 
 class TransferTransactionResponse(
     timestamp: Long,
@@ -10,9 +12,11 @@ class TransferTransactionResponse(
     senderSignature: String,
     senderPublicKey: String,
     hash: String,
+    val status: Boolean,
     val amount: Long,
     val recipientAddress: String? = null,
     val data: String? = null,
+    val results: List<ReceiptResultResponse>? = null,
     blockHash: String? = null
 ) : BaseTransactionResponse(timestamp, fee, senderAddress, senderSignature, senderPublicKey, hash, blockHash) {
 
@@ -23,21 +27,24 @@ class TransferTransactionResponse(
         tx.signature,
         tx.publicKey,
         tx.hash,
+        true,
         tx.getPayload().amount,
         tx.getPayload().recipientAddress,
         tx.getPayload().data
     )
 
-    constructor(tx: TransferTransaction) : this(
+    constructor(tx: TransferTransaction, receipt: Receipt) : this(
         tx.timestamp,
         tx.fee,
         tx.senderAddress,
         tx.signature,
         tx.publicKey,
         tx.hash,
+        receipt.isSuccessful(),
         tx.getPayload().amount,
         tx.getPayload().recipientAddress,
         tx.getPayload().data,
+        receipt.getResults().map { ReceiptResultResponse(it) },
         tx.block?.hash
     )
 
