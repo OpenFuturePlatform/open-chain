92956dcd6e9cb61cbb975b6aa6a414d896ebf2a2
anna.shunko <anna.shunko@zensoft.io>
2019-02-15 15:36:04 +0300

    execute transaction estimation

--- src/main/kotlin/io/openfuture/chain/rpc/controller/SmartContractController.kt
+++ src/main/kotlin/io/openfuture/chain/rpc/controller/SmartContractController.kt
@@ -1,5 +1,9 @@
 package io.openfuture.chain.rpc.controller
 
+import io.openfuture.chain.core.model.entity.dictionary.TransferTransactionType
+import io.openfuture.chain.core.model.entity.dictionary.TransferTransactionType.DEPLOY
+import io.openfuture.chain.core.model.entity.dictionary.TransferTransactionType.EXECUTE
+import io.openfuture.chain.core.service.ContractService
 import io.openfuture.chain.rpc.domain.SmartContractEstimateRequest
 import io.openfuture.chain.smartcontract.deploy.calculation.ContractCostCalculator
 import org.bouncycastle.pqc.math.linearalgebra.ByteUtils.fromHexString
@@ -12,11 +16,16 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/rpc/contracts")
 class SmartContractController(
-    private val contractCostCalculator: ContractCostCalculator
+    private val contractCostCalculator: ContractCostCalculator,
+    private val contractService: ContractService
 ) {
 
     @PostMapping("/estimation")
     fun getEstimation(@RequestBody @Valid request: SmartContractEstimateRequest): Long =
-        contractCostCalculator.calculateCost(fromHexString(request.bytecode))
+        when (TransferTransactionType.getType(request.recipientAddress, request.data)) {
+            DEPLOY -> contractCostCalculator.calculateCost(fromHexString(request.data))
+            EXECUTE -> contractService.getByAddress(request.recipientAddress!!).cost
+            else -> throw IllegalArgumentException("Invalid request")
+        }
 
 }
