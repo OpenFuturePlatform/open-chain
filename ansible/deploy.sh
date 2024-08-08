aa530cbd2598c7e49654561756fd1700bf9ec38f
beksultan <beksultan@openfuture.io>
2024-08-08 20:35:55 +0600

    Migrate to AWS

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -9,11 +9,11 @@
 #ssh -i ansible/key.pem -o StrictHostKeyChecking=no ubuntu@35.236.216.126 'ls -la'
 #ssh -i ansible/key.pem -o StrictHostKeyChecking=no ubuntu@35.199.11.229 'ls -la'
 #ssh -i ansible/key.pem -o StrictHostKeyChecking=no ubuntu@35.194.84.89 'ls -la'
-ssh -i ansible/key.pem -o StrictHostKeyChecking=no ubuntu@34.145.185.53 'ls -la'
+ssh -i ansible/key.pem -o StrictHostKeyChecking=no ec2-user@ec2-54-87-74-220.compute-1.amazonaws.com 'ls -la'
 
 mkdir ansible/host_vars
 #for i in `seq 1 11`; do eval echo "key: \${{ secrets.SERVER${i} }}" >> ansible/host_vars/server${i}; done
 #for i in `seq 1 11`; do eval echo "key: \$SERVER${i}" >> ansible/host_vars/server${i}; done
 # For 2 containers on one host.
 #for i in `seq 12 22`; do j=$(($i - 11)); eval echo "key2: \$SERVER${i}" >> ansible/host_vars/server${j}; done
-ansible-playbook --private-key=ansible/key.pem -i ansible/inventory -f 11 -e "del_volume=${DELETE_VOLUME}" -e "image_tag=${IMAGE_TAG}" -e "count=11" -e "user_login=ubuntu" ansible/deploy.yml
+ansible-playbook --private-key=ansible/key.pem -i ansible/inventory -f 11 -e "del_volume=${DELETE_VOLUME}" -e "image_tag=${IMAGE_TAG}" -e "count=11" -e "user_login=ec2-user" ansible/deploy.yml
